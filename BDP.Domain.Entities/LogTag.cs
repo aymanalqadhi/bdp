768064e19ebfb97b9ebe4de3089e0fbf368c262f
aymanalqadhi <alqd@protonmail.com>
2022-06-01 15:36:07 +0300

    Add an entity for log tags

---
+++ BDP.Domain.Entities/LogTag.cs
@@ -0,0 +1,21 @@
+﻿namespace BDP.Domain.Entities;
+
+/// <summary>
+/// A class to represent a log tag
+/// </summary>
+public sealed class LogTag : AuditableEntity
+{
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the collection of logs associated with this tag
+    /// </summary>
+    public ICollection<Log> Logs { get; set; } = new List<Log>();
+
+    /// <summary>
+    /// Gets or sets the message of the log
+    /// </summary>
+    public string Value { get; set; } = null!;
+
+    #endregion Properties
+}
