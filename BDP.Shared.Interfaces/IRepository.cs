1a5e80670a5009cda110f89ed6d451fa5e396198
aymanalqadhi <alqd@protonmail.com>
2022-06-01 15:57:52 +0300

    Add an interface to represent the new compact repository

---
+++ BDP.Shared.Interfaces/IRepository.cs
@@ -0,0 +1,34 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Domain.Repositories;
+
+public interface IRepository<T> where T : AuditableEntity
+{
+    #region Public Methods
+
+    /// <summary>
+    /// Adds an item to the repository
+    /// </summary>
+    /// <param name="entities">The items to add</param>
+    void Add(params T[] entities);
+
+    /// <summary>
+    /// Creates a query builder for <see cref="T"/>
+    /// </summary>
+    /// <returns>The query builder object</returns>
+    IQueryBuilder<T> Query();
+
+    /// <summary>
+    /// Deletes an item from the repositry
+    /// </summary>
+    /// <param name="entity">The item to delete</param>
+    void Remove(T entity);
+
+    /// <summary>
+    /// Updates an item in the repository
+    /// </summary>
+    /// <param name="entity">The updated item</param>
+    void Update(T entity);
+
+    #endregion Public Methods
+}
