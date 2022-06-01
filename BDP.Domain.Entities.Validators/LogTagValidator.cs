cf20d5c2af4207d0ba144af3de306daf7956ce95
aymanalqadhi <alqd@protonmail.com>
2022-06-01 15:36:27 +0300

    Add a validator class for LogTag entity

---
+++ BDP.Domain.Entities.Validators/LogTagValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="LogTag"/>
+/// </summary>
+public sealed class LogTagValidator : Validator<LogTag>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public LogTagValidator()
+    {
+        RuleFor(l => l.Value)
+            .NotEmpty()
+            .WithMessage("log tag value cannot be empty");
+    }
+}
