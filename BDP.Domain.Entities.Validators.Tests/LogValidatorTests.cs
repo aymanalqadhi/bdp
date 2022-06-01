8866058d7ae59738bd4520b91a350574453167b9
aymanalqadhi <alqd@protonmail.com>
2022-06-01 15:37:29 +0300

    Test the validator class of the Log entity

---
+++ BDP.Domain.Entities.Validators.Tests/LogValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using BDP.Domain.Entities;
+using BDP.Domain.Entities.Validators;
+using BDP.Tests.Utils;
+
+using FluentValidation.TestHelper;
+
+using Xunit;
+
+namespace BDP.Tests.Domain.Entities.Validators;
+
+public class LogValidatorTests
+{
+    #region Private fields
+
+    private readonly LogValidator _validator = new();
+
+    #endregion Private fields
+
+    #region Tests
+
+    [Fact]
+    public void ValidateMessageShouldSucceed()
+    {
+        var log = new Log
+        {
+            Message = RandomGenerator.NextString(0xff)
+        };
+
+        Assert.True(_validator.Validate(log).IsValid);
+    }
+
+    [Fact]
+    public void ValidateMessageShouldFail()
+    {
+        var log = new Log { Message = "" };
+
+        var result = _validator.TestValidate(log);
+
+        result.ShouldHaveValidationErrorFor(l => l.Message);
+    }
+
+    #endregion Tests
+}
