140973cb09efb6917c40f7ec2f8d5e1a8eac5eda
aymanalqadhi <alqd@protonmail.com>
2022-06-01 16:00:43 +0300

    Add a utlity static class to create an in-memory testing database context

---
+++ BDP.Infrastructure.Repository.EntityFramework.Tests/Util/TestDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BDP.Infrastructure.Repositories.EntityFramework;
+using BDP.Tests.Utils;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace BDP.Tests.Infrastructure.Repositories.EntityFramework.Util;
+
+public static class TestDbContext
+{
+    #region Public Methods
+
+    /// <summary>
+    /// Creates an in-memory database context
+    /// </summary>
+    /// <returns></returns>
+    public static BdpDbContext Create()
+    {
+        var options = new DbContextOptionsBuilder<BdpDbContext>()
+            .UseInMemoryDatabase($"test_apss_db_{RandomGenerator.NextString(16)}")
+            .ConfigureWarnings((o) => o.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+        return new BdpDbContext(options);
+    }
+
+    #endregion Public Methods
+}
