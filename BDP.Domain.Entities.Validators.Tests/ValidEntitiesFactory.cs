4fb3273750cc73878c63d822db24e8c54f2ce147
aymanalqadhi <alqd@protonmail.com>
2022-06-01 15:37:44 +0300

    Add a utility class to create valid entity objects

---
+++ BDP.Domain.Entities.Validators.Tests/ValidEntitiesFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace BDP.Domain.Entities.Validators.Tests;
+
+public static class ValidEntitiesFactory
+{
+    /// <summary>
+    /// Creates a valid log entites
+    /// </summary>
+    /// <returns></returns>
+    public static Log CreateLog()
+    {
+        return new Log
+        {
+            Severity = LogSeverity.Error,
+        };
+    }
+}
