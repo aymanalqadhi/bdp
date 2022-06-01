5f1847cdaf0280eb90255eb8d8797d31d72ba5ad
aymanalqadhi <alqd@protonmail.com>
2022-06-01 15:37:15 +0300

    Add a validator class for the Log entity

---
+++ BDP.Domain.Entities.Validators/LogValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Log"/>
+/// </summary>
+public sealed class LogValidator : Validator<Log>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public LogValidator()
+    {
+        RuleFor(l => l.Message)
+            .NotEmpty()
+            .WithMessage("log message cannot be empty");
+    }
+}
