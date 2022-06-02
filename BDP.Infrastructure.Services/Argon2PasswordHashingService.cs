6a871f01ec1844999668def779eb9d187ffa4b06
aymanalqadhi <alqd@protonmail.com>
2022-06-02 21:09:17 +0300

    Implement an Argon2 password hashing service

---
+++ BDP.Infrastructure.Services/Argon2PasswordHashingService.cs
@@ -0,0 +1,15 @@
+﻿using BDP.Domain.Services;
+using Isopoh.Cryptography.Argon2;
+
+namespace BDP.Infrastructure.Services;
+
+public sealed class Argon2PasswordHashingService : IPasswordHashingService
+{
+    /// <inheritdoc/>
+    public string Hash(string password)
+        => Argon2.Hash(password);
+
+    /// <inheritdoc/>
+    public bool Verify(string password, string hash)
+        => Argon2.Verify(hash, password);
+}
