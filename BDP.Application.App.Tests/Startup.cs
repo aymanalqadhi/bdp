bc45ca4b3e588b35b2c47fc5ee04ee686a7511a6
aymanalqadhi <alqd@protonmail.com>
2022-06-04 17:46:42 +0300

    Use the new unit of work interface and class in services tests

---
+++ BDP.Application.App.Tests/Startup.cs
@@ -0,0 +1,30 @@
+﻿using BDP.Domain.Repositories;
+using BDP.Domain.Services;
+using BDP.Infrastructure.Repositories.EntityFramework;
+using BDP.Tests.Infrastructure.Repositories.EntityFramework.Util;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BDP.Application.App.Tests;
+
+public class Startup
+{
+    public void ConfigurationServices(IServiceCollection services)
+    {
+        services.AddTransient(_ => TestDbContext.Create());
+        services.AddTransient<IUnitOfWork, BdpUnitOfWork>();
+
+        services.AddTransient<IAuthService, AuthService>();
+        services.AddTransient<IUsersService, UsersService>();
+        services.AddTransient<IFinancialRecordsService, FinancialRecordsService>();
+        services.AddTransient<ITransactionsService, TransactionsService>();
+        services.AddTransient<IFinanceService, FinanceService>();
+        services.AddTransient<ISellablesService, SellablesService>();
+        services.AddTransient<ISellableReviewsService, SellableReviewsService>();
+        services.AddTransient<IProductsService, ProductsService>();
+        services.AddTransient<IServicesService, ServicesService>();
+        services.AddTransient<IPurchasesService, PurchasesService>();
+        services.AddTransient<ISearchSuggestionsService, SearchSuggestionsService>();
+        services.AddTransient<IEventsService, EventsService>();
+    }
+}
