0abda778cc00e1f11b000c15786fc11200666610
aymanalqadhi <alqd@protonmail.com>
2022-06-04 19:57:36 +0300

    Add an exception to be thrown when no items are found

---
+++ BDP.Shared.Interfaces/Exceptions/NotFoundException.cs
@@ -0,0 +1,33 @@
+﻿namespace BDP.Domain.Repositories.Exceptions;
+
+public sealed class NotFoundException : Exception
+{
+    #region Fields
+
+    private readonly Exception? _innerExcpetion;
+
+    #endregion Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="message">A message that clarifies the error cause</param>
+    public NotFoundException(string message, Exception? innerException = null)
+        : base(message)
+    {
+        _innerExcpetion = innerException;
+    }
+
+    #endregion Public Constructors
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the inner exception
+    /// </summary>
+    public Exception? InnerExcpetion => _innerExcpetion;
+
+    #endregion Properties
+}
