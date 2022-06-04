7c7d690c93409e43600c00a01b33944c488cc8c2
aymanalqadhi <alqd@protonmail.com>
2022-06-04 17:47:01 +0300

    Add a factory method to create a valid user entity object

--- BDP.Domain.Entities.Validators.Tests/ValidEntitiesFactory.cs
+++ BDP.Domain.Entities.Validators.Tests/ValidEntitiesFactory.cs
@@ -1,9 +1,26 @@
-﻿namespace BDP.Domain.Entities.Validators.Tests;
+﻿using BDP.Tests.Util;
+
+namespace BDP.Domain.Entities.Validators.Tests;
 
 public static class ValidEntitiesFactory
 {
     /// <summary>
-    /// Creates a valid log entites
+    /// Creates a valid user object
+    /// </summary>
+    /// <returns></returns>
+    public static User CreateUser()
+    {
+        var alphaTextBlock = (int max) => RandomGenerator.NextString(max, RandomStringOptions.Lowercase);
+
+        return new User
+        {
+            Username = alphaTextBlock(8),
+            Email = $"{alphaTextBlock(10)}@{alphaTextBlock(6)}.{alphaTextBlock(5)}",
+        };
+    }
+
+    /// <summary>
+    /// Creates a valid log object
     /// </summary>
     /// <returns></returns>
     public static Log CreateLog()
