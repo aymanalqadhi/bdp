a4bc859fc810c88fe05d64bc8ff65719ada1323d
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:41 +0300

    Fix typo in AttachmentValidator class name

--- BDP.Domain.Entities.Validators/AttachmentValidator.cs
+++ BDP.Domain.Entities.Validators/AttachmentValidator.cs
@@ -5,14 +5,15 @@ namespace BDP.Domain.Entities.Validators;
 /// <summary>
 /// A validator for the entity <see cref="Attachment"/>
 /// </summary>
-public class AttachmentsValidator : Validator<Attachment>
+public class AttachmentValidator : Validator<Attachment>
 {
     /// <summary>
     /// Default constructor
     /// </summary>
-    public AttachmentsValidator()
+    public AttachmentValidator()
     {
         RuleFor(a => a.Mime)
-            .Matches(@"(.+?)/(.+?)");
+            .Matches(@"(.+?)/(.+?)")
+            .WithMessage("invalid MIME type");
     }
 }
