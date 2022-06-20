d0199b31ac80072c134735ef178a5c313952ecab
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 01:54:57 +0300

    Add a method to parse roles from string values

--- BDP.Web.Api/Auth/UserRoleConverter.cs
+++ BDP.Web.Api/Auth/UserRoleConverter.cs
@@ -1,24 +1,55 @@
 ﻿using BDP.Domain.Entities;
+using BDP.Web.Api.Exceptions;
 
 namespace BDP.Web.Api.Auth;
 
 public static class UserRoleConverter
 {
+    #region Fields
+
+    private const string _adminRole = "admin";
+    private const string _customerRole = "customer";
+    private const string _providerRole = "provider";
+    private const string _rootRole = "root";
+
+    #endregion Fields
+
+    #region Public Methods
+
     /// <summary>
     /// Converts a role value to string
     /// </summary>
     /// <param name="role">The role value</param>
     /// <returns></returns>
     public static string FromRole(UserRole role)
-    {
-        return role switch
+        => role switch
         {
-            UserRole.Root => "root",
-            UserRole.Admin => "admin",
-            UserRole.Customer => "customer",
-            UserRole.Provider => "provider",
+            UserRole.Root => _rootRole,
+            UserRole.Admin => _adminRole,
+            UserRole.Customer => _customerRole,
+            UserRole.Provider => _providerRole,
 
             _ => string.Empty,
         };
-    }
+
+    /// <summary>
+    /// Parses a role from string representation
+    /// </summary>
+    /// <param name="role">the role to parse</param>
+    /// <returns></returns>
+    /// <exception cref="InvalidRoleStringException">
+    /// Thrown when an invalid role string value is used
+    /// </exception>
+    public static UserRole Parse(string role)
+        => role switch
+        {
+            _rootRole => UserRole.Root,
+            _adminRole => UserRole.Admin,
+            _customerRole => UserRole.Customer,
+            _providerRole => UserRole.Provider,
+
+            _ => throw new InvalidRoleStringException(role),
+        };
+
+    #endregion Public Methods
 }
