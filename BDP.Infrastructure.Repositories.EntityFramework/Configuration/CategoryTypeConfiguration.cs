56292ef4de476b3ad61d20a6edfa0eb0cb90f541
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:41 +0300

    Add a type configuration class for Category entity

---
+++ BDP.Infrastructure.Repositories.EntityFramework/Configuration/CategoryTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using BDP.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BDP.Infrastructure.Repositories.EntityFramework.Configuration;
+
+/// <inheritdoc/>
+public class CategoryTypeConfiguration : EntityTypeConfiguration<Category>
+{
+    /// <inheritdoc/>
+    public override void Configure(EntityTypeBuilder<Category> builder)
+    {
+        base.Configure(builder);
+
+        // indeces
+        builder.HasIndex(c => c.Name).IsUnique();
+    }
+}
