a484cdedae9c6f3ff89b7000509777046029db35
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:17 +0300

    Use a strongly-typed key for PhoneNumber entity

--- BDP.Domain.Entities/PhoneNumber.cs
+++ BDP.Domain.Entities/PhoneNumber.cs
@@ -1,10 +1,21 @@
 ﻿namespace BDP.Domain.Entities;
 
 /// <summary>
+/// A recrod to represent keys for the <see cref="PhoneNumber"/> entity
+/// </summary>
+/// <param name="Id">The id field of the key</param>
+public sealed record PhoneNumberKey(Guid Id) : EntityKey<PhoneNumber>(Id);
+
+/// <summary>
 /// A class to represent a phone number owned by a user
 /// </summary>
 public sealed class PhoneNumber : AuditableEntity
 {
+    /// <summary>
+    /// Gets or sets the id of the phone number
+    /// </summary>
+    public PhoneNumberKey Id { get; set; } = new PhoneNumberKey(Guid.NewGuid());
+
     /// <summary>
     /// Gets or sets the phone number value
     /// </summary>
