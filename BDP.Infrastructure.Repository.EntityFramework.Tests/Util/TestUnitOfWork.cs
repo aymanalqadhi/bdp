261f4f33d40ef1bdcc76d60179de92c4863a4a9a
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:39:53 +0300

    Create a utility testing class to create a testing unit of work

---
+++ BDP.Infrastructure.Repository.EntityFramework.Tests/Util/TestUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using BDP.Domain.Repositories;
+using BDP.Infrastructure.Repositories.EntityFramework;
+
+namespace BDP.Tests.Infrastructure.Repositories.EntityFramework.Util;
+
+public static class TestUnitOfWork
+{
+    #region Public Methods
+
+    /// <summary>
+    /// Creates a testing unit of work object
+    /// </summary>
+    /// <returns>The created object</returns>
+    public static IUnitOfWork Create(BdpDbContext? context = null)
+        => new BdpUnitOfWork(context ?? TestDbContext.Create());
+
+    #endregion Public Methods
+}
