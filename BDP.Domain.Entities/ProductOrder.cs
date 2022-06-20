92520207c785a3e0dfeca7406c9ecc0126861f02
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:30 +0300

    Add a product order purchase type

--- BDP.Domain.Entities/ProductOrder.cs
+++ BDP.Domain.Entities/ProductOrder.cs
@@ -3,13 +3,8 @@
 /// <summary>
 /// A class to represent a product order
 /// </summary>
-public sealed class ProductOrder : Purchase<ProductOrder>
+public sealed class ProductOrder : Purchase<ProductOrder, Product>
 {
-    /// <summary>
-    /// Gets or sets the product of the order
-    /// </summary>
-    public Product Product { get; set; } = null!;
-
     /// <summary>
     /// Gets or sets the ordered quantity
     /// </summary>
