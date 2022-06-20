dc0ee09cd3a1cb3b28ca5b1029a7ca49e76f8201
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:29 +0300

    Add a product sellable type

--- BDP.Domain.Entities/Product.cs
+++ BDP.Domain.Entities/Product.cs
@@ -3,10 +3,15 @@
 /// <summary>
 /// A class to represent a product sold by a user
 /// </summary>
-public sealed class Product : Sellable<Product, ProductReview>
+public sealed class Product : Sellable<Product>
 {
     /// <summary>
     /// Gets or sets the available quantity of the product
     /// </summary>
     public uint Quantity { get; set; }
+
+    /// <summary>
+    /// Gets or sets the collection of reviews
+    /// </summary>
+    public ICollection<ProductReview> Reviews { get; set; } = new List<ProductReview>();
 }
