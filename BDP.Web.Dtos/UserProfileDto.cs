3cc300a4501d4e71b74150e4e6913f49d43afeef
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:57 +0300

    Add a dto for user profiles

---
+++ BDP.Web.Dtos/UserProfileDto.cs
@@ -0,0 +1,49 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Web.Dtos;
+
+/// <summary>
+/// A data-transfer object for <see cref="UserProfile"/>
+/// </summary>
+public sealed class UserProfileDto : MutableEntityDto<UserProfile>
+{
+    /// <summary>
+    /// Gets or sets the full name of the user
+    /// </summary>
+    public string FullName { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the user's bio
+    /// </summary>
+    public string? Bio { get; set; }
+
+    /// <summary>
+    /// Gets or sets the location of the user
+    /// </summary>
+    public string? Address { get; set; }
+
+    /// <summary>
+    /// Gets or sets the phone number of the user
+    /// </summary>
+    public string? PhoneNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the user has been verified
+    /// </summary>
+    public bool IsVerified { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets the profile picture of the user
+    /// </summary>
+    public Uri? ProfilePicture { get; set; }
+
+    /// <summary>
+    /// Gets or sets the cover picture of the user
+    /// </summary>
+    public Uri? CoverPicture { get; set; }
+
+    /// <summary>
+    /// Gets or sets the owner of the profile
+    /// </summary>
+    public UserDto User { get; set; } = null!;
+}
