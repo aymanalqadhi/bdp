2ae7ab74dc44ff5d33b304836475933fee45c210
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:20 +0300

    Use the generic strongly-typed key for EventType entity

--- BDP.Domain.Entities/EventType.cs
+++ BDP.Domain.Entities/EventType.cs
@@ -1,21 +1,10 @@
 ﻿namespace BDP.Domain.Entities;
 
 /// <summary>
-/// A recrod to represent keys for the <see cref="EventType"/> entity
-/// </summary>
-/// <param name="Id">The id field of the key</param>
-public sealed record EventTypeKey(Guid Id) : EntityKey<EventType>(Id);
-
-/// <summary>
 /// A class to represent an event type
 /// </summary>
-public class EventType : AuditableEntity
+public class EventType : AuditableEntity<EventType>
 {
-    /// <summary>
-    /// Gets or sets the id of the event type
-    /// </summary>
-    public EventTypeKey Id { get; set; } = new EventTypeKey(Guid.NewGuid());
-
     /// <summary>
     /// Gets or sets the name of the event type
     /// </summary>
