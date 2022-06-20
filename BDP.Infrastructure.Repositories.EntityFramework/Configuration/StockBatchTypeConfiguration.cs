3bfb31bc0e9c43b781ca80ea647c96ab38a4384d
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:46 +0300

    Add a type configuration class for StockBatch entity

---
+++ BDP.Infrastructure.Repositories.EntityFramework/Configuration/StockBatchTypeConfiguration.cs
@@ -0,0 +1,8 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Infrastructure.Repositories.EntityFramework.Configuration;
+
+/// <inheritdoc/>
+public sealed class StockBatchTypeConfiguration : EntityTypeConfiguration<StockBatch>
+{
+}
