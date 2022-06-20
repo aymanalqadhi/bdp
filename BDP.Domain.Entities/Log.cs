6ddb0685040e8df013bddccc37af8f421ab4ccaa
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:39:49 +0300

    Add an entity to represent logs

---
+++ BDP.Domain.Entities/Log.cs
@@ -0,0 +1,43 @@
+﻿namespace BDP.Domain.Entities;
+
+/// <summary>
+/// A class to represent a log
+/// </summary>
+public sealed class Log : AuditableEntity
+{
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the message of the log
+    /// </summary>
+    public string Message { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the log severity level
+    /// </summary>
+    public LogSeverity Severity { get; set; }
+
+    /// <summary>
+    /// Gets or sets the tags set of the log
+    /// </summary>
+    public ICollection<LogTag> Tags { get; set; } = new List<LogTag>();
+
+    /// <summary>
+    /// Gets or sets the timestamp of the log
+    /// </summary>
+    public DateTime TimeStamp { get; set; }
+
+    #endregion Properties
+}
+
+/// <summary>
+/// An enum to represent the log severity levels
+/// </summary>
+public enum LogSeverity : int
+{
+    Fatal = 0,
+    Error = 1,
+    Warning = 2,
+    Information = 3,
+    Debug = 4,
+}
