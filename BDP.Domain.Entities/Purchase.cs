a0c43b3d68bbe5aff926175d7d73ee12bdc209c2
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:17 +0300

    Use a strongly-typed key for Purchase entity

--- BDP.Domain.Entities/Purchase.cs
+++ BDP.Domain.Entities/Purchase.cs
@@ -1,7 +1,18 @@
 ﻿namespace BDP.Domain.Entities;
 
+/// <summary>
+/// A recrod to represent keys for the <see cref="Purchase"/> entity
+/// </summary>
+/// <param name="Id">The id field of the key</param>
+public sealed record PurchaseKey(Guid Id) : EntityKey<Purchase>(Id);
+
 public class Purchase : AuditableEntity
 {
+    /// <summary>
+    /// Gets or sets the id of the purchase
+    /// </summary>
+    public PurchaseKey Id { get; set; } = new PurchaseKey(Guid.NewGuid());
+
     /// <summary>
     /// Gets or sets the transaction associated with this purchase
     /// </summary>
