fbacc1e672082a1022b573370ab0388647ea97a2
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:36 +0300

    Rewrite ProductOrder entity

---
+++ BDP.Domain.Entities/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace BDP.Domain.Entities;
+
+public sealed class Order : Purchase<Order, SellableVariant>
+{
+    /// <summary>
+    /// Gets or sets the quantity of the order
+    /// </summary>
+    public uint Quantity { get; set; }
+}
