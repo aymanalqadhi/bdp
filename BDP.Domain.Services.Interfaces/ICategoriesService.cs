94030bb882d21b9b4d4413abb2b23215ecd3b5cd
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:48 +0300

    Add an interface for product categories

---
+++ BDP.Domain.Services.Interfaces/ICategoriesService.cs
@@ -0,0 +1,24 @@
+﻿using BDP.Domain.Entities;
+using BDP.Domain.Repositories;
+
+namespace BDP.Domain.Services;
+
+/// <summary>
+/// An service to manage product categories
+/// </summary>
+public interface ICategoriesService
+{
+    /// <summary>
+    /// Gets product categories
+    /// </summary>
+    /// <returns>A query builder for product categories</returns>
+    IQueryBuilder<Category> GetCategories();
+
+    /// <summary>
+    /// Asynchronously adds a product cateogry
+    /// </summary>
+    /// <param name="name">the name of the category</param>
+    /// <param name="parent">The parent of the category (optional)</param>
+    /// <returns>The created category object</returns>
+    Task<Category> AddAsync(string name, Category? parent = null);
+}
