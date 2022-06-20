e6f78229ae941b3f6513b382f82901a5d525de17
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:56 +0300

    Add a data-transfer object base class based on EntityDto with modification date

---
+++ BDP.Web.Dtos/MutableEntityDto.cs
@@ -0,0 +1,17 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Web.Dtos;
+
+/// <summary>
+/// A base data transfer object based on <see cref="EntityDto{TEntity}"/> with an
+/// additional modification date
+/// </summary>
+/// <typeparam name="TEntity">The entity type</typeparam>
+public abstract class MutableEntityDto<TEntity> : EntityDto<TEntity>
+    where TEntity : AuditableEntity<TEntity>
+{
+    /// <summary>
+    /// Gets or sets the modifying date of the entity
+    /// </summary>
+    public DateTime ModifiedAt { get; set; }
+}
