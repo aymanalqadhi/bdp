a78350666c2f59b31fed4ff9b41df906be0c881f
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:42 +0300

    Update old purchase type configuration classes

--- BDP.Infrastructure.Repositories.EntityFramework/Configuration/OrderTypeConfiguration.cs
+++ BDP.Infrastructure.Repositories.EntityFramework/Configuration/OrderTypeConfiguration.cs
@@ -1,22 +1,21 @@
 ﻿using BDP.Domain.Entities;
-
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BDP.Infrastructure.Repositories.EntityFramework.Configuration;
 
 /// <inheritdoc/>
-public class ServiceReservationTypeConfiguration : EntityTypeConfiguration<ServiceReservation>
+public class OrderTypeConfiguration : EntityTypeConfiguration<Order>
 {
     /// <inheritdoc/>
-    public override void Configure(EntityTypeBuilder<ServiceReservation> builder)
+    public override void Configure(EntityTypeBuilder<Order> builder)
     {
         base.Configure(builder);
 
         // relationships
         builder
-            .HasOne(s => s.Item)
-            .WithMany()
-            .OnDelete(DeleteBehavior.Restrict);
+             .HasOne(o => o.Variant)
+             .WithMany()
+             .OnDelete(DeleteBehavior.Restrict);
     }
 }
--- BDP.Infrastructure.Repositories.EntityFramework/Configuration/ReservationTypeConfiguration.cs
+++ BDP.Infrastructure.Repositories.EntityFramework/Configuration/ReservationTypeConfiguration.cs
@@ -1,21 +1,22 @@
 ﻿using BDP.Domain.Entities;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BDP.Infrastructure.Repositories.EntityFramework.Configuration;
 
 /// <inheritdoc/>
-public class ProductOrderTypeConfiguration : EntityTypeConfiguration<ProductOrder>
+public class ReservationTypeConfiguration : EntityTypeConfiguration<Reservation>
 {
     /// <inheritdoc/>
-    public override void Configure(EntityTypeBuilder<ProductOrder> builder)
+    public override void Configure(EntityTypeBuilder<Reservation> builder)
     {
         base.Configure(builder);
 
         // relationships
         builder
-             .HasOne(o => o.Item)
-             .WithMany()
-             .OnDelete(DeleteBehavior.Restrict);
+            .HasOne(s => s.Variant)
+            .WithMany()
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
