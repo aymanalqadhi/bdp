6d4932e524d5482b9f2ed8cd9e8d3be091cbcd1b
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:50 +0300

    Rewrite NotEnoughStockException to use the new strongly-typed keys

--- BDP.Application.App/Exceptions/NotEnoughStockException.cs
+++ BDP.Application.App/Exceptions/NotEnoughStockException.cs
@@ -2,30 +2,46 @@
 
 namespace BDP.Application.App.Exceptions;
 
+/// <summary>
+/// An exception to be thrown when a quantity that does not exist is requsted
+/// from a sellable product variant
+/// </summary>
 public class NotEnoughStockException : Exception
 {
-    private readonly Product _product;
+    #region Fields
+
     private readonly uint _requestedQuantity;
+    private readonly EntityKey<ProductVariant> _variantid;
+
+    #endregion Fields
+
+    #region Public Constructors
 
     /// <summary>
     /// Default constructor
     /// </summary>
-    /// <param name="product">The product lacking stock</param>
+    /// <param name="variantid">The product lacking stock</param>
     /// <param name="requestedQuantity">The requested quantity</param>
-    public NotEnoughStockException(Product product, uint requestedQuantity)
-        : base($"product #{product.Id} does not have {requestedQuantity} stock left")
+    public NotEnoughStockException(EntityKey<ProductVariant> variantid, uint requestedQuantity)
+        : base($"product variant #{variantid.Id} does not have {requestedQuantity} stock left")
     {
-        _product = product;
+        _variantid = variantid;
         _requestedQuantity = requestedQuantity;
     }
+
+    #endregion Public Constructors
+
+    #region Properties
 
     /// <summary>
     /// Gets the product associated with this exception
     /// </summary>
-    public Product Product => _product;
+    public EntityKey<ProductVariant> ProductVariantId => _variantid;
 
     /// <summary>
     /// Gets the requested quantity
     /// </summary>
     public uint RequestedQuantity => _requestedQuantity;
+
+    #endregion Properties
 }
