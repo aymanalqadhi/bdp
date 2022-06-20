66c6d153845b12ff18d4d4ea5f14d592ae239e09
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:38 +0300

    Create a validator for EventType entity

---
+++ BDP.Domain.Entities.Validators/EventTypeValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="EventType"/>
+/// </summary>
+public sealed class EventTypeValidator : Validator<EventType>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public EventTypeValidator()
+    {
+        RuleFor(e => e.Name)
+            .NotEmpty()
+            .WithMessage("event type name is required");
+    }
+}
