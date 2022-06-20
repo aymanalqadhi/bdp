b45f0d9c7edbf6eedcaed86753a3d4c10a4bd0e5
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:41 +0300

    Add a class for product category entity

---
+++ BDP.Domain.Entities/Category.cs
@@ -0,0 +1,27 @@
+﻿namespace BDP.Domain.Entities;
+
+/// <summary>
+/// An class that represents a products cateogry
+/// </summary>
+public sealed class Category : AuditableEntity<Category>
+{
+    /// <summary>
+    /// Gets or sets the name of the category
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the parent (if exists) of the category
+    /// </summary>
+    public Category? Parent { get; set; }
+
+    /// <summary>
+    /// Gets or sets the collection of subcategories of this category
+    /// </summary>
+    public ICollection<Category> Children { get; set; } = new List<Category>();
+
+    /// <summary>
+    /// Gets or sets the collection of products in this category
+    /// </summary>
+    public ICollection<Product> Products { get; set; } = new List<Product>();
+}
