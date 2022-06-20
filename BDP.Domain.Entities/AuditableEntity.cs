b72f0c0a7d80948814e20c141efd26d434ce1976
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:19 +0300

    Restore the Id property with a generic parameter passed (inheriting entity)

--- BDP.Domain.Entities/AuditableEntity.cs
+++ BDP.Domain.Entities/AuditableEntity.cs
@@ -3,8 +3,13 @@
 /// <summary>
 /// A class to represent a domain entity with autition
 /// </summary>
-public abstract class AuditableEntity
+public abstract class AuditableEntity<T> where T : class
 {
+    /// <summary>
+    /// Gets or sets the id of the entity
+    /// </summary>
+    public EntityKey<T> Id { get; set; } = new EntityKey<T>(Guid.NewGuid());
+
     /// <summary>
     /// Gets or sets the creation date of the entity
     /// </summary>
