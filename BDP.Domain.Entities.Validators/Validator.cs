bed8efcd0fa8deb0576e885c7ee72f5d64494625
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:35 +0300

    Implemented a base validator

--- BDP.Domain.Entities.Validators/Validator.cs
+++ BDP.Domain.Entities.Validators/Validator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using BDP.Domain.Entities.Validators.Exceptions;
+using FluentValidation;
 
 namespace BDP.Domain.Entities.Validators;
 
@@ -6,6 +7,24 @@ namespace BDP.Domain.Entities.Validators;
 /// Base validator type
 /// </summary>
 /// <typeparam name="T">The type which to validate</typeparam>
-public class Validator<T> : AbstractValidator<T>
+public class Validator<T> : AbstractValidator<T>, IValidator<T>
 {
+    /// <inheritdoc/>
+    public bool IsValid(T item)
+        => Validate(item).IsValid;
+
+    /// <inheritdoc/>
+    void IValidator<T>.Validate(T item)
+    {
+        var res = Validate(item);
+
+        if (!res.IsValid)
+        {
+            throw new ValidationAggregateException(
+                res.Errors.Select(e =>
+                    new ValidationError(e.PropertyName, e.AttemptedValue, e.ErrorMessage)
+                )
+            );
+        }
+    }
 }
