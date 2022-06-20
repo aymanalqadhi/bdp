3a12c0d7adfe0918d0d76f27d9d6d8f631f4adc6
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:39 +0300

    Add a validator for ProductReview entity

---
+++ BDP.Domain.Entities.Validators/ProductReviewValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="ProductReview"/>
+/// </summary>
+public sealed class ProductReviewValidator : Validator<ProductReview>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public ProductReviewValidator()
+    {
+        RuleFor(c => c.Rating)
+            .Must(c => c >= 1 && c <= 5)
+            .WithMessage("rating value should be in the range 1-5");
+    }
+}
