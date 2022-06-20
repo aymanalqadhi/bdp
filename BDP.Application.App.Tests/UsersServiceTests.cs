695898b979b33cb997baf0bf53555cfe4bc23670
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:39:56 +0300

    Use the new unit of work interface and class in services tests

---
+++ BDP.Application.App.Tests/UsersServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using BDP.Application.App.Exceptions;
+using BDP.Domain.Entities.Validators.Tests;
+using BDP.Domain.Repositories;
+using BDP.Domain.Services;
+using BDP.Infrastructure.Repositories.EntityFramework;
+using BDP.Tests.Util;
+
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace BDP.Application.App.Tests;
+
+public class UsersServiceTests
+{
+    private readonly IUnitOfWork _uow;
+    private readonly IUsersService _usersSvc;
+
+    public UsersServiceTests(IUnitOfWork uow, IUsersService usersService)
+    {
+        _uow = uow;
+        _usersSvc = usersService;
+    }
+
+    [Fact]
+    public async Task FindByUsernameFact()
+    {
+        var user = ValidEntitiesFactory.CreateUser();
+
+        _uow.Users.Add(user);
+        await _uow.CommitAsync();
+
+        Assert.True(await _uow.Users.Query().AnyAsync(u => u.Id == user.Id));
+
+        var found = await _usersSvc.GetByUsernameAsync(user.Username);
+
+        Assert.Equal(user.Id, found.Id);
+        Assert.Equal(user.Username, found.Username);
+
+        await Assert.ThrowsAsync<NotFoundException>(async () =>
+        {
+            await _usersSvc.GetByUsernameAsync(user.Username + RandomGenerator.NextString(2));
+        });
+    }
+}
