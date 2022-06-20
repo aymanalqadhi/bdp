725e94f527152a7a3cb0b8fecf6d0632a6d3a5d1
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:56 +0300

    Add a base data-transfer object class

---
+++ BDP.Web.Dtos/EntityDto.cs
@@ -0,0 +1,21 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Web.Dtos;
+
+/// <summary>
+/// A base data transfer object class
+/// </summary>
+/// <typeparam name="TEntity">The entity type</typeparam>
+public abstract class EntityDto<TEntity>
+    where TEntity : AuditableEntity<TEntity>
+{
+    /// <summary>
+    /// Gets or sets the id of the entity
+    /// </summary>
+    public EntityKey<TEntity> Id { get; set; } = new EntityKey<TEntity>(Guid.NewGuid());
+
+    /// <summary>
+    /// Gets or sets the creation date of the entity
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+}
