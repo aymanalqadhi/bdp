5f30acee2b6aa33929dd8a8995317ed5b5edc056
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:40:07 +0300

    Add a utility mapping extension to map an entity to a dto

---
+++ BDP.Web.Api/Extensions/IQueryBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using BDP.Domain.Repositories;
+
+using AutoMapper;
+
+namespace BDP.Web.Api.Extensions;
+
+public static class IQueryBuilderExtensions
+{
+    /// <summary>
+    /// A utility method to map a <see cref="IQueryBuilder{T}"/> using AutoMapper
+    /// </summary>
+    /// <typeparam name="T">The source type</typeparam>
+    /// <typeparam name="V">The destination type</typeparam>
+    /// <param name="source">The source query builder</param>
+    /// <param name="mapper">The mapper object</param>
+    /// <returns>The modified query builder object</returns>
+    public static IQueryBuilder<V> Map<T, V>(this IQueryBuilder<T> source, IMapper mapper)
+        where T : class
+        where V : class
+    {
+        return source.Select(i => mapper.Map<V>(i));
+    }
+}
