547cec08857098934dc634db8a729dded460b9f0
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:14 +0300

    Change ITransactionsService methods object parameters to Guid id's

--- BDP.Domain.Services.Interfaces/ITransactionsService.cs
+++ BDP.Domain.Services.Interfaces/ITransactionsService.cs
@@ -18,46 +18,46 @@ public interface ITransactionsService
     /// Asynchrnously gets transactions that a user has either sent or recieved
     /// limited by pagination
     /// </summary>
-    /// <param name="user">The user which to get transactions for</param>
+    /// <param name="userId">The id of the user which to get transactions for</param>
     /// <returns></returns>
-    IQueryBuilder<Transaction> ForUserAsync(User user);
+    IQueryBuilder<Transaction> ForUserAsync(Guid userId);
 
     /// <summary>
     /// Asynchronously gets the total input transferred to the user
     /// </summary>
-    /// <param name="user">The user to get the total for</param>
+    /// <param name="userId">The id of the user to get the total for</param>
     /// <param name="confirmedOnly">If true, only confirmed transactions will count</param>
     /// <returns>Total account input</returns>
-    Task<decimal> TotalInAsync(User user, bool confirmedOnly = false);
+    Task<decimal> TotalInAsync(Guid userId, bool confirmedOnly = false);
 
     /// <summary>
     /// Asynchronously gets the total output transferred by the user
     /// </summary>
-    /// <param name="user">The user to get the total for</param>
+    /// <param name="userId">The id of the user to get the total for</param>
     /// <param name="confirmedOnly">If true, only confirmed transactions will count</param>
     /// <returns>Total account output</returns>
-    Task<decimal> TotalOutAsync(User user, bool confirmedOnly = false);
+    Task<decimal> TotalOutAsync(Guid userId, bool confirmedOnly = false);
 
     /// <summary>
     /// Asynchronously gets the total usable balance for the user
     /// </summary>
-    /// <param name="user">The user to get the total for</param>
+    /// <param name="userId">The id of the user to get the total for</param>
     /// <returns>Total usable credit</returns>
-    Task<decimal> TotalUsableAsync(User user);
+    Task<decimal> TotalUsableAsync(Guid userId);
 
     /// <summary>
     /// Asynchronously confirms a transaction
     /// </summary>
-    /// <param name="sender">The sender of the transaction</param>
+    /// <param name="senderId">The id of the sender of the transaction</param>
     /// <param name="confirmationToken">The token to confirm with</param>
     /// <returns>The transaction confirmation</returns>
-    Task<TransactionConfirmation> ConfirmAsync(User sender, string confirmationToken);
+    Task<TransactionConfirmation> ConfirmAsync(Guid senderId, string confirmationToken);
 
     /// <summary>
     /// Asynchronously cancels a transaction
     /// </summary>
-    /// <param name="receiver">The receiver of the transaction</param>
+    /// <param name="receiverId">The id of the receiver of the transaction</param>
     /// <param name="transactionId">The id of the transaction to cancel</param>
     /// <returns>The canceled transaction confirmation</returns>
-    Task<TransactionConfirmation> CancelAsync(User receiver, Guid transactionId);
+    Task<TransactionConfirmation> CancelAsync(Guid receiverId, Guid transactionId);
 }
