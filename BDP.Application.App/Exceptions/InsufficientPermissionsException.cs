a83a0c3349b8d451a23daeec18df9d6a6df571e2
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:50 +0300

    Add an exception to be thrown when a user requests a resource with insufficient permissions

---
+++ BDP.Application.App/Exceptions/InsufficientPermissionsException.cs
@@ -0,0 +1,40 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Application.App.Exceptions;
+
+/// <summary>
+/// An exception to be thrown when a user with insufficient permissions
+/// attempts to do a privleaged operation
+/// </summary>
+public sealed class InsufficientPermissionsException : Exception
+{
+    #region Fields
+
+    private readonly EntityKey<User> _userId;
+
+    #endregion Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="userId">The id of the user who lacks permissions</param>
+    /// <param name="message">(optional) a message to clarify the error</param>
+    public InsufficientPermissionsException(EntityKey<User> userId, string? message = null)
+        : base(message)
+    {
+        _userId = userId;
+    }
+
+    #endregion Public Constructors
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the id of the user who lacks sufficient permissions
+    /// </summary>
+    public EntityKey<User> UserId => _userId;
+
+    #endregion Properties
+}
