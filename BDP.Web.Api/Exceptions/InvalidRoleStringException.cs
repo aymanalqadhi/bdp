2676d23fb412c0960295e516d663c74a2ab6f1f5
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 01:55:18 +0300

    Add an exception to be thrown when an invalid role string is parsed

---
+++ BDP.Web.Api/Exceptions/InvalidRoleStringException.cs
@@ -0,0 +1,36 @@
+﻿namespace BDP.Web.Api.Exceptions;
+
+/// <summary>
+/// An exception to be thrown when an invalid role string representaion is used
+/// </summary>
+public sealed class InvalidRoleStringException : Exception
+{
+    #region Fields
+
+    private readonly string _roleString;
+
+    #endregion Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="roleString">The invalid role string value</param>
+    public InvalidRoleStringException(string roleString)
+        : base($"invalid role string: {roleString}")
+    {
+        _roleString = roleString;
+    }
+
+    #endregion Public Constructors
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the invalid role string valu
+    /// </summary>
+    public string RoleString => _roleString;
+
+    #endregion Properties
+}
