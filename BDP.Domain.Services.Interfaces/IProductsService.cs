2bfd0062cbed7934263c97b7a5737a976f05d2e4
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:13 +0300

    Change IProductsService methods object parameters to Guid id's

--- BDP.Domain.Services.Interfaces/IProductsService.cs
+++ BDP.Domain.Services.Interfaces/IProductsService.cs
@@ -14,7 +14,7 @@ public interface IProductsService
     /// <summary>
     /// Asynchronously lists a product
     /// </summary>
-    /// <param name="user">The user which to list the product for</param>
+    /// <param name="userId">The id of the user which to list the product for</param>
     /// <param name="title">The title of the product</param>
     /// <param name="description">The description of the product</param>
     /// <param name="price">The price of the product</param>
@@ -22,7 +22,7 @@ public interface IProductsService
     /// <param name="attachments">The file attachments of the product</param>
     /// <returns>The listed product</returns>
     Task<Product> ListAsync(
-        User user,
+        Guid userId,
         string title,
         string description,
         decimal price,
@@ -33,13 +33,13 @@ public interface IProductsService
     /// <summary>
     /// Asynchronsoulsy updates a product
     /// </summary>
-    /// <param name="product">The product to update</param>
+    /// <param name="productId">The id of the product to update</param>
     /// <param name="title">The new title of the product</param>
     /// <param name="description">The new description of the product</param>
     /// <param name="price">The new price of the product</param>
     /// <returns></returns>
     Task<Product> UpdateAsync(
-        Product product,
+        Guid productId,
         string title,
         string description,
         decimal price
@@ -48,39 +48,23 @@ public interface IProductsService
     /// <summary>
     /// Asynchrnously unlists a product
     /// </summary>
-    /// <param name="product"></param>
+    /// <param name="productId">The id of the product to unlist</param>
     /// <returns></returns>
-    Task UnlistAsync(Product product);
-
-    /// <summary>
-    /// Asynchronously adds to the stock of a product
-    /// </summary>
-    /// <param name="product">The procut to add to its stock</param>
-    /// <param name="quantity">The quantity to add</param>
-    /// <returns>The updated product</returns>
-    Task<Product> AddStock(Product product, uint quantity);
-
-    /// <summary>
-    /// Asynchronously removes from the stock of a product
-    /// </summary>
-    /// <param name="product">The procut to remove fromto its stock</param>
-    /// <param name="quantity">The quantity to remove</param>
-    /// <returns>The updated product</returns>
-    Task<Product> RemoveStock(Product product, uint quantity);
+    Task UnlistAsync(Guid productId);
 
     /// <summary>
     /// Asynchrnously gets the available quantity of the product
     /// </summary>
-    /// <param name="product">The product to check for</param>
+    /// <param name="productId">The id of the product to check for</param>
     /// <returns>The available quantity</returns>
-    Task<long> AvailableQuantityAsync(Product product);
+    Task<long> AvailableQuantityAsync(Guid productId);
 
     /// <summary>
     /// Asynchrnously checks whether the product is available
     /// </summary>
-    /// <param name="product">The product to check for</param>
+    /// <param name="productId">The id of the product to check for</param>
     /// <returns>True if the product has an available qauntity, false otherwise</returns>
-    Task<bool> IsAvailableAsync(Product product);
+    Task<bool> IsAvailableAsync(Guid productId);
 
     /// <summary>
     /// Asynchronously changes the availability flag of a product
@@ -88,14 +72,14 @@ public interface IProductsService
     /// <param name="product">The product to change</param>
     /// <param name="isAvailable">The new availability value</param>
     /// <returns>The updated service</returns>
-    Task<Product> SetAvailability(Product product, bool isAvailable);
+    Task<Product> SetAvailability(Guid productId, bool isAvailable);
 
     /// <summary>
     /// Asynchronously creates an order for a product
     /// </summary>
-    /// <param name="by">The user which to create the order for</param>
-    /// <param name="product">The product to order</param>
+    /// <param name="userId">The id of the user which to create the order for</param>
+    /// <param name="productId">The id of the product to order</param>
     /// <param name="quantity">The ordered quantity</param>
     /// <returns>The created order</returns>
-    Task<ProductOrder> OrderAsync(User by, Product product, uint quantity);
+    Task<ProductOrder> OrderAsync(Guid userId, Guid productId, uint quantity);
 }
