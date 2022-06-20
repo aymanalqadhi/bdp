34719c06ddb7e6483312efe02060b710060ba9f0
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:28 +0300

    Remove profile-specific fields from the class

--- BDP.Domain.Entities/User.cs
+++ BDP.Domain.Entities/User.cs
@@ -21,26 +21,6 @@ public sealed class User : AuditableEntity<User>
     public string PasswordHash { get; set; } = null!;
 
     /// <summary>
-    /// Gets or sets the full name of the user
-    /// </summary>
-    public string? FullName { get; set; }
-
-    /// <summary>
-    /// Gets or sets the user's bio
-    /// </summary>
-    public string? Bio { get; set; }
-
-    /// <summary>
-    /// Gets or sets the location of the user
-    /// </summary>
-    public string? Location { get; set; }
-
-    /// <summary>
-    /// Gets or sets the phone number of the user
-    /// </summary>
-    public string? PhoneNumber { get; set; }
-
-    /// <summary>
     /// Gets or sets whether the user is active or not
     /// </summary>
     public bool IsActive { get; set; } = false;
@@ -51,24 +31,9 @@ public sealed class User : AuditableEntity<User>
     public bool IsConfirmed { get; set; } = false;
 
     /// <summary>
-    /// Gets or sets whether the user has been verified
-    /// </summary>
-    public bool IsVerified { get; set; } = false;
-
-    /// <summary>
     /// Gets or sets the role of the user
     /// </summary>
     public UserRole Role { get; set; } = UserRole.None;
-
-    /// <summary>
-    /// Gets or sets the profile picture of the user
-    /// </summary>
-    public Attachment? ProfilePicture { get; set; }
-
-    /// <summary>
-    /// Gets or sets the cover picture of the user
-    /// </summary>
-    public Attachment? CoverPicture { get; set; }
 }
 
 /// <summary>
