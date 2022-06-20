c962644f08986174215333b891c10c873624aca2
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:40 +0300

    Add a validator for Reservation entity

---
+++ BDP.Domain.Entities.Validators/ReservationValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Reservation"/>
+/// </summary>
+public sealed class ReservationValidator : Validator<Reservation>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public ReservationValidator()
+    {
+    }
+}
