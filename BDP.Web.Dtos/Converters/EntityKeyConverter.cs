aeafc26ff694c5d6c0510904031b33cca22916aa
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:56 +0300

    Add a type converter for strongly-typed ids

---
+++ BDP.Web.Dtos/Converters/EntityKeyConverter.cs
@@ -0,0 +1,41 @@
+﻿using BDP.Domain.Entities;
+
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Collections.Concurrent;
+
+namespace BDP.Web.Dtos.Converters;
+
+/// <inheritdoc/>
+public class StronglyTypedIdJsonConverter<TEntity, TValue>
+    : JsonConverter<EntityKey<TEntity, TValue>>
+{
+    /// <inheritdoc/>
+    public override EntityKey<TEntity, TValue>? Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options)
+    {
+        if (reader.TokenType is JsonTokenType.Null)
+            return null;
+
+        var value = JsonSerializer.Deserialize<TValue>(ref reader, options);
+
+        return new EntityKey<TEntity, TValue>(value!);
+    }
+
+    /// <inheritdoc/>
+    public override void Write(Utf8JsonWriter writer, EntityKey<TEntity, TValue> value, JsonSerializerOptions options)
+    {
+        if (value is null)
+            writer.WriteNullValue();
+        else
+            JsonSerializer.Serialize(writer, value.Id, options);
+    }
+}
+
+/// <inheritdoc/>
+public class StronglyTypedIdJsonConverter<TEntity>
+    : StronglyTypedIdJsonConverter<TEntity, Guid>
+{
+}
