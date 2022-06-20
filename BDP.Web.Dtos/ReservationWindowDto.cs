687121eefe7eb1d46f223aa16b2df0fff194bcb4
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:58 +0300

    Add a data-transfer object for ReservationWindow

---
+++ BDP.Web.Dtos/ReservationWindowDto.cs
@@ -0,0 +1,24 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Web.Dtos;
+
+/// <summary>
+/// A data-transfer object for <see cref="ProductVariant"/> with sellable type
+/// </summary>
+public sealed class ReservationWindowDto : MutableEntityDto<ReservationWindow>
+{
+    /// <summary>
+    /// Gets or sets the available days of the reservable product
+    /// </summary>
+    public byte AvailableDays { get; set; }
+
+    /// <summary>
+    /// Gets or sets the starting time of reservable product availability
+    /// </summary>
+    public TimeSpan Start { get; set; }
+
+    /// <summary>
+    /// Gets or sets the ending time of reservable product availability
+    /// </summary>
+    public TimeSpan End { get; set; }
+}
