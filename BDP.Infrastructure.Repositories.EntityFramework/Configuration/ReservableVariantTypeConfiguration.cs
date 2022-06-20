f38e0c4f6664fac2e88c369681fb339187293429
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:46 +0300

    Remove type configuration classes for deleted entities

--- BDP.Infrastructure.Repositories.EntityFramework/Configuration/ReservableVariantTypeConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿using BDP.Domain.Entities;
-
-namespace BDP.Infrastructure.Repositories.EntityFramework.Configuration;
-
-/// <inheritdoc/>
-public sealed class ReservableVariantTypeConfiguration : ProductVariantTypeConfiguration<ReservableVariant>
-{
-}
