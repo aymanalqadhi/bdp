42d7b8065335533e9fbe8154356c5c3b0e77ee60
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:36 +0300

    Merge sellable reviews into one entity

--- BDP.Domain.Entities/ProductReview.cs
+++ BDP.Domain.Entities/ProductReview.cs
@@ -1,8 +1,24 @@
 ﻿namespace BDP.Domain.Entities;
 
-/// <summary>
-/// A class to represent a product review
-/// </summary>
-public sealed class ProductReview : SellableReview<ProductReview, Product>
+public sealed class ProductReview : AuditableEntity<ProductReview>
 {
+    /// <summary>
+    /// Gets or sets teh rating value
+    /// </summary>
+    public double Rating { get; set; }
+
+    /// <summary>
+    /// Gets or sets the comment left by the reviewer
+    /// </summary>
+    public string? Comment { get; set; }
+
+    /// <summary>
+    /// Gets or sets the user who left the review
+    /// </summary>
+    public User LeftBy { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the reviews product
+    /// </summary>
+    public Product Product { get; set; } = null!;
 }
--- BDP.Domain.Entities/ServiceReview.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BDP.Domain.Entities;
-
-/// <summary>
-/// A class to represent a service review
-/// </summary>
-public sealed class ServiceReview : SellableReview<ServiceReview, Service>
-{ }
