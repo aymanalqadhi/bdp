aa5a76597e2f2f706575eb62157188750a8e40f2
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:44 +0300

    Add an entity to represent stock batches for sellable product variants

---
+++ BDP.Domain.Entities/StockBatch.cs
@@ -0,0 +1,12 @@
+﻿namespace BDP.Domain.Entities;
+
+/// <summary>
+/// A class to rerperesnt sellable product stock batch entity
+/// </summary>
+public sealed class StockBatch : AuditableEntity<StockBatch>
+{
+    /// <summary>
+    /// Gets or sets the quantity of the stock batch
+    /// </summary>
+    public uint Quantity { get; set; } = 0;
+}
