d3da392dfdc2214ff360819a1733d40aa7f0fbb7
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:18 +0300

    Use a strongly-typed key for Sellalbe entity

--- BDP.Domain.Entities/Sellable.cs
+++ BDP.Domain.Entities/Sellable.cs
@@ -1,11 +1,22 @@
 ﻿namespace BDP.Domain.Entities;
 
 /// <summary>
+/// A recrod to represent keys for the <see cref="Sellable"/> entity
+/// </summary>
+/// <param name="Id">The id field of the key</param>
+public sealed record SellableKey(Guid Id) : EntityKey<Sellable>(Id);
+
+/// <summary>
 /// A class to represent a sellable item
 /// </summary>
 /// <typeparam name="TKey">The key type of the sellable item</typeparam>
 public class Sellable : AuditableEntity
 {
+    /// <summary>
+    /// Gets or sets the id of the sellable item
+    /// </summary>
+    public SellableKey Id { get; set; } = new SellableKey(Guid.NewGuid());
+
     /// <summary>
     /// Gets or sets the title of the sellable
     /// </summary>
