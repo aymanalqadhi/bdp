efeaa945bf7b49a64d56fdfe52889181af6db1c4
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:39 +0300

    Validate the note field of FinancialRecord entity

--- BDP.Domain.Entities.Validators/FinancialRecordVerificationValidator.cs
+++ BDP.Domain.Entities.Validators/FinancialRecordVerificationValidator.cs
@@ -3,19 +3,15 @@
 namespace BDP.Domain.Entities.Validators;
 
 /// <summary>
-/// A validator for the entity <see cref="FinancialRecord"/>
+/// A validator for the entity <see cref="FinancialRecordVerification"/>
 /// </summary>
-public sealed class FinancialRecordValidator : Validator<FinancialRecord>
+public sealed class FinancialRecordVerificationValidator : Validator<FinancialRecordVerification>
 {
     /// <summary>
     /// Default constructor
     /// </summary>
-    public FinancialRecordValidator()
+    public FinancialRecordVerificationValidator()
     {
-        RuleFor(f => f.Amount)
-            .NotEqual(0)
-            .WithMessage("financial record amount cannot be 0");
-
         RuleFor(f => f.Note)
             .MaximumLength(255)
             .When(n => n is not null)
