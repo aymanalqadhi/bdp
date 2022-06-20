031f7db315308d3e0b95e362deb1f304ee76d59c
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:50 +0300

    Rewrite events service

--- BDP.Application.App/EventsService.cs
+++ BDP.Application.App/EventsService.cs
@@ -8,51 +8,44 @@ using System.Linq.Expressions;
 
 namespace BDP.Application.App;
 
+/// <inheritdoc/>
 public class EventsService : IEventsService
 {
-    #region Private fields
+    #region Fields
 
-    private readonly IUnitOfWork _uow;
     private readonly IAttachmentsService _attachmentsSvc;
+    private readonly IUnitOfWork _uow;
 
-    #endregion Private fields
+    #endregion Fields
 
-    #region Ctors
+    #region Public Constructors
 
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="uow">The unit of work of the application</param>
+    /// <param name="attachmentsSvc">Attachments managment service</param>
     public EventsService(IUnitOfWork uow, IAttachmentsService attachmentsSvc)
     {
         _uow = uow;
         _attachmentsSvc = attachmentsSvc;
     }
 
-    #endregion Ctors
+    #endregion Public Constructors
 
-    #region Public methods
+    #region Public Methods
 
     /// <inheritdoc/>
-    public async Task<Event> GetByIdAsync(EntityKey<Event> id)
+    public async Task AddImageAsync(EntityKey<Event> eventId, IUploadFile image)
     {
-        var @event = await _uow.Events.Query()
-            .Include(e => e.Purchases)
-            .Include(e => e.Pictures)
-            .Include(e => e.Type)
-            .Include(e => e.CreatedBy)
-            .FirstAsync(e => e.Id == id);
+        var @event = await _uow.Events.Query().FindAsync(eventId);
+        var attachment = await _attachmentsSvc.SaveAsync(image);
 
-        return @event;
+        @event.Pictures.Add(attachment);
+        _uow.Events.Update(@event);
+
+        await _uow.CommitAsync();
     }
-
-    /// <inheritdoc/>
-    public Task<EventType> GetTypeByIdAsync(EntityKey<EventType> id)
-        => _uow.EventTypes.Query().FirstAsync(e => e.Id == id);
-
-    /// <inheritdoc/>
-    public IQueryBuilder<EventType> GetEventTypes()
-        => _uow.EventTypes.Query();
-
-    /// <inheritdoc/>
-    public IQueryBuilder<Event> ForUserAsync(EntityKey<User> userId)
-        => _uow.Events.Query().Where(e => e.CreatedBy.Id == userId);
 
     /// <inheritdoc/>
     public async Task<Event> CreateAsync(
@@ -82,6 +75,31 @@ public class EventsService : IEventsService
     }
 
     /// <inheritdoc/>
+    public IQueryBuilder<Event> ForUser(EntityKey<User> userId)
+        => GetEvents().Where(e => e.CreatedBy.Id == userId);
+
+    /// <inheritdoc/>
+    public IQueryBuilder<Event> ForUserAsync(EntityKey<User> userId)
+        => _uow.Events.Query().Where(e => e.CreatedBy.Id == userId);
+
+    /// <inheritdoc/>
+    public IQueryBuilder<Event> GetEvents()
+        => _uow.Events.Query();
+
+    /// <inheritdoc/>
+    public IQueryBuilder<EventType> GetEventTypes()
+        => _uow.EventTypes.Query();
+
+    /// <inheritdoc/>
+    public async Task RemoveAsync(EntityKey<Event> eventId)
+    {
+        var @event = await _uow.Events.Query().FindAsync(eventId);
+
+        _uow.Events.Remove(@event);
+        await _uow.CommitAsync();
+    }
+
+    /// <inheritdoc/>
     public async Task<Event> UpdateAsync(
         EntityKey<Event> eventId,
         EntityKey<EventType> typeId,
@@ -104,39 +122,6 @@ public class EventsService : IEventsService
     }
 
     /// <inheritdoc/>
-    public async Task RemoveAsync(EntityKey<Event> eventId)
-    {
-        var @event = await _uow.Events.Query().FindAsync(eventId);
-
-        _uow.Events.Remove(@event);
-        await _uow.CommitAsync();
-    }
-
-    /// <inheritdoc/>
-    public async Task AssociatePurchaseAsync(EntityKey<Event> eventId, EntityKey<Purchase> purchaseId)
-    {
-        var @event = await _uow.Events.Query().FindAsync(eventId);
-        var purchase = await _uow.Purchases.Query().FindAsync(purchaseId);
-
-        @event.Purchases.Add(purchase);
-        _uow.Events.Update(@event);
-
-        await _uow.CommitAsync();
-    }
-
-    /// <inheritdoc/>
-    public async Task AddImageAsync(EntityKey<Event> eventId, IUploadFile image)
-    {
-        var @event = await _uow.Events.Query().FindAsync(eventId);
-        var attachment = await _attachmentsSvc.SaveAsync(image);
-
-        @event.Pictures.Add(attachment);
-        _uow.Events.Update(@event);
-
-        await _uow.CommitAsync();
-    }
-
-    /// <inheritdoc/>
     public async Task UpdateProgressAsync(EntityKey<Event> eventId, double progress)
     {
         if (progress > 1 || progress < 0)
@@ -150,5 +135,5 @@ public class EventsService : IEventsService
         await _uow.CommitAsync();
     }
 
-    #endregion Public methods
+    #endregion Public Methods
 }
