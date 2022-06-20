f16e88af87c8f79c0d9720fe4a6ec20627efdb49
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:28 +0300

    Create a separate class for user profiles

---
+++ BDP.Domain.Entities/UserProfile.cs
@@ -0,0 +1,49 @@
+﻿namespace BDP.Domain.Entities;
+
+public class UserProfile : AuditableEntity<UserProfile>
+{
+    /// <summary>
+    /// Gets or sets the full name of the user
+    /// </summary>
+    public string? FullName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the user's bio
+    /// </summary>
+    public string? Bio { get; set; }
+
+    /// <summary>
+    /// Gets or sets the location of the user
+    /// </summary>
+    public string? Location { get; set; }
+
+    /// <summary>
+    /// Gets or sets the phone number of the user
+    /// </summary>
+    public string? PhoneNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the user has been verified
+    /// </summary>
+    public bool IsVerified { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets the profile picture of the user
+    /// </summary>
+    public Attachment? ProfilePicture { get; set; }
+
+    /// <summary>
+    /// Gets or sets the cover picture of the user
+    /// </summary>
+    public Attachment? CoverPicture { get; set; }
+
+    /// <summary>
+    /// Gets or sets the id of the owner of the profile
+    /// </summary>
+    public EntityKey<User> UserId { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the owner of the profile
+    /// </summary>
+    public User User { get; set; } = null!;
+}
