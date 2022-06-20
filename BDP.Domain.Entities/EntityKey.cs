25d6be1af50541640363056e86ec588769798a08
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:47 +0300

    Propagate inner id type string

--- BDP.Domain.Entities/EntityKey.cs
+++ BDP.Domain.Entities/EntityKey.cs
@@ -1,3 +1,7 @@
 ﻿namespace BDP.Domain.Entities;
 
-public sealed record EntityKey<TEntity>(Guid Id);
+public sealed record EntityKey<TEntity>(Guid Id)
+{
+    /// <inheritdoc/>
+    public override string ToString() => Id.ToString();
+}
