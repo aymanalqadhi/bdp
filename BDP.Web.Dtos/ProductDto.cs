3bc37541647d3caa9201dd43d174594482a007ce
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:57 +0300

    Rewrite SellableDto data-transfer object as ProductDto

--- BDP.Web.Dtos/ProductDto.cs
+++ BDP.Web.Dtos/ProductDto.cs
@@ -1,9 +1,34 @@
-﻿namespace BDP.Web.Dtos;
+﻿using BDP.Domain.Entities;
 
-public class ProductDto : SellableDto
+namespace BDP.Web.Dtos;
+
+/// <summary>
+/// A data-transfer object for <see cref="Product"/>
+/// </summary>
+public sealed class ProductDto : MutableEntityDto<Product>
 {
     /// <summary>
-    /// Gets or sets the available quantity of the product
+    /// Gets or sets the title of the sellable
     /// </summary>
-    public uint Quantity { get; set; }
+    public string Title { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the description of the sellable
+    /// </summary
+    public string Description { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the user who listed this sellable
+    /// </summary>
+    public UserDto OfferedBy { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets whether the sellable is available
+    /// </summary>
+    public bool IsAvailable { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets the collection of categories the product belongs to
+    /// </summary>
+    public IEnumerable<string> Categories { get; set; } = new List<string>();
 }
