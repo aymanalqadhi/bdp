f2a7f46a55d39775e6e4a3f336cd516377b3264f
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:39 +0300

    Add a validator for Product entity

---
+++ BDP.Domain.Entities.Validators/OrderValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Order"/>
+/// </summary>
+public sealed class OrderValidator : Validator<Order>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public OrderValidator()
+    {
+        RuleFor(o => o.Quantity)
+            .LessThanOrEqualTo(o => o.Variant.AvailableQuantity)
+            .WithMessage("order quantity cannot be greater than the available quantity");
+    }
+}
