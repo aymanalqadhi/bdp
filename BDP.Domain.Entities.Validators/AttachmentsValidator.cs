a742cbf664dd2b06c4ffa3b8952c615dbc8e1896
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:37 +0300

    Validate mime types of attachments

---
+++ BDP.Domain.Entities.Validators/AttachmentsValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Attachment"/>
+/// </summary>
+public class AttachmentsValidator : Validator<Attachment>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public AttachmentsValidator()
+    {
+        RuleFor(a => a.Mime)
+            .Matches(@"(.+?)/(.+?)");
+    }
+}
