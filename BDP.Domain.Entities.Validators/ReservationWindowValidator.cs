00e6ff489f10bea51054ef7fec4013a4c6b31697
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:44 +0300

    Add a validator for ReservationWindow entity

---
+++ BDP.Domain.Entities.Validators/ReservationWindowValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="ReservationWindow"/>
+/// </summary>
+public sealed class ReservationWindowValidator : Validator<ReservationWindow>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public ReservationWindowValidator()
+    {
+    }
+}
