81431c63744c9bcef5d95b6c8039cf2203f9c7b9
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:53 +0300

    Rewrite IPurchasesService interface

--- BDP.Domain.Services.Interfaces/IPurchasesService.cs
+++ BDP.Domain.Services.Interfaces/IPurchasesService.cs
@@ -13,26 +13,12 @@ public interface IPurchasesService
     #region Public Methods
 
     /// <summary>
-    /// Asynchronously gets the available quantity for a sellable product variant
-    /// </summary>
-    /// <param name="variantId">The id of the sellable product variant</param>
-    /// <returns>The avaiable quantity</returns>
-    Task<uint> AvailableSellableVariantQuantityAsync(EntityKey<ProductVariant> variantId);
-
-    /// <summary>
-    /// Asynchronously checks if a product variant can be purchased or not
-    /// </summary>
-    /// <param name="variantId">The id of the product to check for</param>
-    /// <returns>True if the product variant is available for purchase, false otherwise</returns>
-    Task<bool> CanPurchaseAsync(EntityKey<ProductVariant> variantId);
-
-    /// <summary>
     /// Asynchronously makes an order for a sellable product variant
     /// </summary>
-    /// <param name="userId"></param>
-    /// <param name="variantId"></param>
-    /// <param name="quantity"></param>
-    /// <returns></returns>
+    /// <param name="userId">The id of the user to make the order for</param>
+    /// <param name="variantId">The product variant to order</param>
+    /// <param name="quantity">The quantity to be ordered</param>
+    /// <returns>The created order</returns>
     Task<Order> OrderAsync(
         EntityKey<User> userId,
         EntityKey<ProductVariant> variantId,
@@ -41,12 +27,47 @@ public interface IPurchasesService
     /// <summary>
     /// Asynchronously makes a reservation for a reservable product variant
     /// </summary>
-    /// <param name="userId"></param>
-    /// <param name="variantId"></param>
-    /// <returns></returns>
-    Task<Order> ReserveAsync(
+    /// <param name="userId">The id of the user to make the reservation for</param>
+    /// <param name="variantId">The product variant to reserve</param>
+    /// <returns>The created reservation</returns>
+    Task<Reservation> ReserveAsync(
         EntityKey<User> userId,
         EntityKey<ProductVariant> variantId);
+
+    /// <summary>
+    /// Asynchronously checks whether a product has pending purchases or not
+    /// </summary>
+    /// <param name="productId">The id of the product to check for</param>
+    /// <returns>True if the product has pending purchases, false otherwise</returns>
+    Task<bool> HasPendingPurchasesAsync(EntityKey<Product> productId);
+
+    /// <summary>
+    /// Gets pending orders for all sellable variants of a specific product
+    /// </summary>
+    /// <param name="productId">The id of the product to get pending orders for</param>
+    /// <returns></returns>
+    IQueryBuilder<Order> PendingOrders(EntityKey<Product> productId);
+
+    /// <summary>
+    /// Gets pending reservations for all reservable variants of a specific product
+    /// </summary>
+    /// <param name="productId">The id of the product to get pending reservations for</param>
+    /// <returns></returns>
+    IQueryBuilder<Reservation> PendingReservations(EntityKey<Product> productId);
+
+    /// <summary>
+    /// Gets all orders for all sellable variants of a specific product
+    /// </summary>
+    /// <param name="productId">The id of the product to get pending orders for</param>
+    /// <returns></returns>
+    IQueryBuilder<Order> GetOrders(EntityKey<Product> productId);
+
+    /// <summary>
+    /// Gets all reservations for all reservable variants of a specific product
+    /// </summary>
+    /// <param name="productId">The id of the product to get pending reservations for</param>
+    /// <returns></returns>
+    IQueryBuilder<Reservation> GetReservations(EntityKey<Product> productId);
 
     #endregion Public Methods
 }
