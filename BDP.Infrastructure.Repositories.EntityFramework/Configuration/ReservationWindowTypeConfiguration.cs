1c5f126de5c8f58aaa25e67805a88380c6a37864
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:46 +0300

    Add a type configuration class for ReservationWindow entity

---
+++ BDP.Infrastructure.Repositories.EntityFramework/Configuration/ReservationWindowTypeConfiguration.cs
@@ -0,0 +1,8 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Infrastructure.Repositories.EntityFramework.Configuration;
+
+/// <inheritdoc/>
+public sealed class ReservationWindowTypeConfiguration : EntityTypeConfiguration<ReservationWindow>
+{
+}
