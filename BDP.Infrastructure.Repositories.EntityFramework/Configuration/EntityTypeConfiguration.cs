2403e7e8204af94f26c299f90e1fb20d168ea2c6
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:31 +0300

    Create a base type configuration class that configures entity key by default

---
+++ BDP.Infrastructure.Repositories.EntityFramework/Configuration/EntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using BDP.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BDP.Infrastructure.Repositories.EntityFramework.Configuration;
+
+/// <inheritdoc/>
+public abstract class EntityTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+    where TEntity : AuditableEntity<TEntity>
+{
+    /// <inheritdoc/>
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder
+            .Property(e => e.Id)
+            .HasConversion(i => i.Id, g => new EntityKey<TEntity>(g));
+    }
+}
