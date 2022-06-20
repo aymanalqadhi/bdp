a0db71d70b406828debb3a2d74436d1022bcd6cf
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:44 +0300

    Validate product variant price

--- BDP.Domain.Entities.Validators/ProductVariantValidator.cs
+++ BDP.Domain.Entities.Validators/ProductVariantValidator.cs
@@ -3,10 +3,9 @@
 namespace BDP.Domain.Entities.Validators;
 
 /// <summary>
-/// A validator for the entity <see cref="ProductVariant{TVariant}"/>
+/// A validator for the entity <see cref="ProductVariant"/>
 /// </summary>
-public abstract class ProductVariantValidator<TVariant> : Validator<TVariant>
-    where TVariant : ProductVariant<TVariant>
+public sealed class ProductVariantValidator : Validator<ProductVariant>
 {
     /// <summary>
     /// Default constructor
@@ -20,5 +19,9 @@ public abstract class ProductVariantValidator<TVariant> : Validator<TVariant>
         RuleFor(s => s.Description)
             .NotEmpty()
             .WithMessage("variant description cannot be empty");
+
+        RuleFor(s => s.Price)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("price cannot be less than 0");
     }
 }
