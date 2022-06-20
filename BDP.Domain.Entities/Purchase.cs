3ce999e8b1c4fc41e6854c5bc7c1c885b941acba
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:30 +0300

    Create a generic purchase base class

--- BDP.Domain.Entities/Purchase.cs
+++ BDP.Domain.Entities/Purchase.cs
@@ -1,10 +1,16 @@
 ﻿namespace BDP.Domain.Entities;
 
-public abstract class Purchase<TEnitity> : AuditableEntity<TEnitity>
+public abstract class Purchase<TEnitity, TSellable> : AuditableEntity<TEnitity>
     where TEnitity : class
+    where TSellable : Sellable<TSellable>
 {
     /// <summary>
     /// Gets or sets the transaction associated with this purchase
     /// </summary>
     public Transaction Transaction { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the purchased item
+    /// </summary>
+    public TSellable Item { get; set; } = null!;
 }
