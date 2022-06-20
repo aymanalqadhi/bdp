e752913c45cc717ee1e564929230c5b8002e3434
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:37 +0300

    Add an entitty to represent a product (sellable) variant

---
+++ BDP.Domain.Entities/ProductVariant.cs
@@ -0,0 +1,30 @@
+﻿namespace BDP.Domain.Entities;
+
+public abstract class ProductVariant<TEntity> : AuditableEntity<TEntity>
+    where TEntity : class
+{
+    /// <summary>
+    /// Gets or sets the name of the variant
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the description of the variant
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the price of the variant
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// Gets the parent product of the variant
+    /// </summary>
+    public Product Product { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the list of attachments of the variant
+    /// </summary>
+    public ICollection<Attachment> Attachments { get; set; } = new List<Attachment>();
+}
