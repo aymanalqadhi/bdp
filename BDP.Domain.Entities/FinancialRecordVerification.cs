6af79e46a5af97edfc8bbda4938624c6ac9a25cd
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:16 +0300

    Use a strongly-typed key for FinancialRecordVerification entity

--- BDP.Domain.Entities/FinancialRecordVerification.cs
+++ BDP.Domain.Entities/FinancialRecordVerification.cs
@@ -1,10 +1,21 @@
 ﻿namespace BDP.Domain.Entities;
 
 /// <summary>
+/// A recrod to represent keys for the <see cref="FinancialRecordVerification"/> entity
+/// </summary>
+/// <param name="Id">The id field of the key</param>
+public sealed record FinancialRecordVerificationKey(Guid Id) : EntityKey<FinancialRecordVerification>(Id);
+
+/// <summary>
 /// A class to represent a financial record verification
 /// </summary>
 public sealed class FinancialRecordVerification : AuditableEntity
 {
+    /// <summary>
+    /// Gets or sets the id of the financial record verification
+    /// </summary>
+    public FinancialRecordVerificationKey Id { get; set; } = new FinancialRecordVerificationKey(Guid.NewGuid());
+
     /// <summary>
     /// Gets or sets the outcome of the financial record verification
     /// </summary>
