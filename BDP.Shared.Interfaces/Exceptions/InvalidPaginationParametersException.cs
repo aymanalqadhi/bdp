936e838f77389d9d27fa579343ae662a1e8467c1
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:39:51 +0300

    Add an expception to be thrown when invalid pagination parameters are used

---
+++ BDP.Shared.Interfaces/Exceptions/InvalidPaginationParametersException.cs
@@ -0,0 +1,41 @@
+﻿namespace BDP.Domain.Repositories.Exceptions;
+
+public class InvalidPaginationParametersException : Exception
+{
+    #region Private fields
+
+    private readonly int _page;
+    private readonly int _pageLength;
+
+    #endregion Private fields
+
+    #region Constructors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="page">The page index</param>
+    /// <param name="pageLength">The page length</param>
+    public InvalidPaginationParametersException(int page, int pageLength)
+        : base($"invalid pagination parameters ({page}, {pageLength})")
+    {
+        _page = page;
+        _pageLength = pageLength;
+    }
+
+    #endregion Constructors
+
+    #region Public properties
+
+    /// <summary>
+    /// Gets the page index
+    /// </summary>
+    public int Page => _page;
+
+    /// <summary>
+    /// Gets the page length
+    /// </summary>
+    public int PageLength => _pageLength;
+
+    #endregion Public properties
+}
