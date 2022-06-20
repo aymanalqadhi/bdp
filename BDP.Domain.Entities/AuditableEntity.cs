b8a7fa9e31261a599bb8cf46759fac0ef7ee3e98
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:16 +0300

    Remove id field from AuditableEntity

--- BDP.Domain.Entities/AuditableEntity.cs
+++ BDP.Domain.Entities/AuditableEntity.cs
@@ -6,11 +6,6 @@
 public abstract class AuditableEntity
 {
     /// <summary>
-    /// Gets or sets the id of the entity
-    /// </summary>
-    public Guid Id { get; set; } = Guid.NewGuid();
-
-    /// <summary>
     /// Gets or sets the creation date of the entity
     /// </summary>
     public DateTime CreatedAt { get; set; }
