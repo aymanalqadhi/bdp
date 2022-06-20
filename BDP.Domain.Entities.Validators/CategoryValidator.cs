88463b7db60cd5eb3696a73534ad08bb1d087cae
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:41 +0300

    Add a validator for Category entity

---
+++ BDP.Domain.Entities.Validators/CategoryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Category"/>
+/// </summary>
+public class CateogryValidator : Validator<Category>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public CateogryValidator()
+    {
+        RuleFor(a => a.Name)
+            .NotEmpty()
+            .WithMessage("category name cannot be empty");
+    }
+}
