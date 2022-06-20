377edfb4ccd48563d55b088f1bdca689e3f56d14
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:40:09 +0300

    Add a class that represents pagination parameters

---
+++ BDP.Web.Dtos/Parameters/PagingParameters.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BDP.Web.Dtos.Parameters;
+
+public sealed class PagingParameters
+{
+    private const int _maxPageLength = 48;
+    private const int _defaultPageLength = 16;
+
+    /// <summary>
+    /// Gets or sets the page index
+    /// </summary>
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int Page { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum page length
+    /// </summary>
+
+    [Range(1, _maxPageLength)]
+    public int PageLength { get; set; } = _defaultPageLength;
+}
