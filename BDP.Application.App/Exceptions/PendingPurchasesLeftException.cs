7e5f68601d11aa90d363b5461071bd41f6c6fae9
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:50 +0300

    Merge pending purchases exceptions into a single exception

---
+++ BDP.Application.App/Exceptions/PendingPurchasesLeftException.cs
@@ -0,0 +1,34 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Application.App.Exceptions;
+
+public sealed class PendingPurchasesLeftException : Exception
+{
+    #region Fields
+
+    private readonly EntityKey<Product> _productId;
+
+    #endregion Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public PendingPurchasesLeftException(EntityKey<Product> productId)
+        : base($"product #{productId} still has pending purchases")
+    {
+        _productId = productId;
+    }
+
+    #endregion Public Constructors
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the id of the product which still has pending purchases
+    /// </summary>
+    public EntityKey<Product> ProductId => _productId;
+
+    #endregion Properties
+}
