29bf413296e66767f08992f5226c5faea6a5090a
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:22 +0300

    Use the generic strongly-typed key for UserGroup entity

--- BDP.Domain.Entities/UserGroup.cs
+++ BDP.Domain.Entities/UserGroup.cs
@@ -1,18 +1,7 @@
 ﻿namespace BDP.Domain.Entities;
 
-/// <summary>
-/// A recrod to represent keys for the <see cref="UserGroup"/> entity
-/// </summary>
-/// <param name="Id">The id field of the key</param>
-public sealed record UserGroupKey(Guid Id) : EntityKey(Id);
-
-public sealed class UserGroup : AuditableEntity
+public sealed class UserGroup : AuditableEntity<UserGroup>
 {
-    /// <summary>
-    /// Gets or sets the id of the user group
-    /// </summary>
-    public UserGroupKey Id { get; set; } = new UserGroupKey(Guid.NewGuid());
-
     /// <summary>
     /// Gets or sets the name of the group
     /// </summary>
