1faacf2ab71ab044866e08bc12710aaad6173757
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:56 +0300

    Add a class to convert UserRole enum values into string constants

---
+++ BDP.Web.Api/Auth/UserRoleConverter.cs
@@ -0,0 +1,24 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Web.Api.Auth;
+
+public static class UserRoleConverter
+{
+    /// <summary>
+    /// Converts a role value to string
+    /// </summary>
+    /// <param name="role">The role value</param>
+    /// <returns></returns>
+    public static string FromRole(UserRole role)
+    {
+        return role switch
+        {
+            UserRole.Root => "root",
+            UserRole.Admin => "admin",
+            UserRole.Customer => "customer",
+            UserRole.Provider => "provider",
+
+            _ => string.Empty,
+        };
+    }
+}
