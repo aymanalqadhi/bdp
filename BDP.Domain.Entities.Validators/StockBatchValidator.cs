ef45fa007cae1eb30525e7387d0aaf28268b5994
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:45 +0300

    Add a validator for StockBatch entity

---
+++ BDP.Domain.Entities.Validators/StockBatchValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="StockBatch"/>
+/// </summary>
+public sealed class StockBatchValidator : Validator<StockBatch>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public StockBatchValidator()
+    {
+    }
+}
