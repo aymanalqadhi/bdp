da0489e3a71ec0bc614c08223ddb20b86f69475b
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:35 +0300

    Create a validation interface

---
+++ BDP.Domain.Entities.Validators/IValidator.cs
@@ -0,0 +1,24 @@
+﻿namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// An interface to be implemented by entity validators
+/// </summary>
+/// <typeparam name="T">The type to be validated</typeparam>
+public interface IValidator<T>
+{
+    /// <summary>
+    /// Validates the passed item
+    /// </summary>
+    /// <param name="item">The item to validate</param>
+    /// <returns>The validation result</returns>
+    bool IsValid(T item);
+
+    /// <summary>
+    /// Validates
+    /// </summary>
+    /// <param name="item"></param>
+    /// <exception cref="ValidationAggregateException">
+    /// Thrown when a validation error is faced
+    /// </exception>
+    void Validate(T item);
+}
