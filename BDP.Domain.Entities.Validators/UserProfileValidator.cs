ecf43a9a61ee29a1a66d679a427559b9bf0cb619
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:37 +0300

    Add a class for user profile validation

---
+++ BDP.Domain.Entities.Validators/UserProfileValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="UserProfile"/>
+/// </summary>
+public class UserProfileValidator : Validator<UserProfile>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public UserProfileValidator()
+    {
+    }
+}
