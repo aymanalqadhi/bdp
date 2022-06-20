4bdf4902812411a38412e2fad2cad7641e02e542
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:39:54 +0300

    Add service for password hashing

---
+++ BDP.Domain.Services.Interfaces/IPasswordHashingService.cs
@@ -0,0 +1,19 @@
+﻿namespace BDP.Domain.Services;
+
+public interface IPasswordHashingService
+{
+    /// <summary>
+    /// Hashes a password using a password-hashing algorithm (e.g., argn2, bcrypt, ...)
+    /// </summary>
+    /// <param name="password">The password to hash</param>
+    /// <returns>The generated hash</returns>
+    string Hash(string password);
+
+    /// <summary>
+    /// Verifies that a password matches the passed hash
+    /// </summary>
+    /// <param name="password">The password to verify</param>
+    /// <param name="hash">The hash to verify against</param>
+    /// <returns></returns>
+    bool Verify(string password, string hash);
+}
