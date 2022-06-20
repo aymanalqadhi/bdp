56fd37c842c71c6216710c258db3026ad0b5407f
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:57 +0300

    Rewrite User data-transfer object

--- BDP.Web.Dtos/UserDto.cs
+++ BDP.Web.Dtos/UserDto.cs
@@ -1,12 +1,12 @@
-﻿namespace BDP.Web.Dtos;
+﻿using BDP.Domain.Entities;
 
-public class UserDto
+namespace BDP.Web.Dtos;
+
+/// <summary>
+/// A data-transfer object for <see cref="User"/>
+/// </summary>
+public sealed class UserDto : EntityDto<User>
 {
-    /// <summary>
-    /// Gets or sets the id of the user
-    /// </summary>
-    public Guid Id { get; set; }
-
     /// <summary>
     /// Gets or sets the username of the user
     /// </summary>
@@ -18,57 +18,12 @@ public class UserDto
     public string Email { get; set; } = null!;
 
     /// <summary>
-    /// Gets or sets the full name of the user
-    /// </summary>
-    public string? FullName { get; set; }
-
-    /// <summary>
-    /// Gets or sets the bio of the user
-    /// </summary>
-    public string? Bio { get; set; }
-
-    /// <summary>
-    /// Gets or sets the location of the user
-    /// </summary>
-    public string? Location { get; set; }
-
-    /// <summary>
     /// Gets or sets whether the user is active or not
     /// </summary>
     public bool IsActive { get; set; } = false;
 
     /// <summary>
-    /// Gets or sets whether the user is active or not
+    /// Gets or sets the role of the user
     /// </summary>
-    public bool IsConfirmed { get; set; } = false;
-
-    /// <summary>
-    /// Gets or sets whether the user has been verified
-    /// </summary>
-    public bool IsVerified { get; set; } = false;
-
-    /// <summary>
-    /// Gets or sets the url to the profile picture of the user
-    /// </summary>
-    public string? ProfilePictureUrl { get; set; }
-
-    /// <summary>
-    /// Gets or sets the creation time of the user
-    /// </summary>
-    public DateTime CreatedAt { get; set; }
-
-    /// <summary>
-    /// Gets or sets the url to the cover picture of the user
-    /// </summary>
-    public string? CoverPictureUrl { get; set; }
-
-    /// <summary>
-    /// Gets or sets the list of phone numbers of the user
-    /// </summary>
-    public ICollection<string>? PhoneNumbers { get; set; }
-
-    /// <summary>
-    /// Gets or sets the list of user groups that the user is enrolled in
-    /// </summary>
-    public ICollection<string> Groups { get; set; } = new List<string>();
+    public string Role { get; set; } = null!;
 }
