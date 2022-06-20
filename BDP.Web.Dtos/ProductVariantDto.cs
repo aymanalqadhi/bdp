909fd47feaada4149d5cc64110f2e40b85f65104
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:58 +0300

    Add a data-transfer object for product variants

---
+++ BDP.Web.Dtos/ProductVariantDto.cs
@@ -0,0 +1,30 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Web.Dtos;
+
+/// <summary>
+/// A data-transfer object for <see cref="ProductVariant"/>
+/// </summary>
+
+public abstract class ProductVariantDto : MutableEntityDto<ProductVariant>
+{
+    /// <summary>
+    /// Gets or sets the name of the variant
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the description of the variant
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the price of the variant
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of attachments of the variant
+    /// </summary>
+    public IEnumerable<Uri> Attachments { get; set; } = new List<Uri>();
+}
---
+++ BDP.Web.Dtos/SellableProductVariantDto.cs
@@ -0,0 +1,15 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Web.Dtos;
+
+/// <summary>
+/// A data-transfer object for <see cref="ProductVariant"/> with sellable type
+/// </summary>
+
+public sealed class SellableProductVariantDto : ProductVariantDto
+{
+    /// <summary>
+    /// Gets or sets the available quantity of the variant
+    /// </summary>
+    public uint AvailableQuantity { get; set; }
+}
