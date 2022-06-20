03813bffff87133c0caa9e6ca03ce2ee21675e0e
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:40 +0300

    Validate UserProfile fields

--- BDP.Domain.Entities.Validators/UserProfileValidator.cs
+++ BDP.Domain.Entities.Validators/UserProfileValidator.cs
@@ -1,4 +1,6 @@
-﻿namespace BDP.Domain.Entities.Validators;
+﻿using FluentValidation;
+
+namespace BDP.Domain.Entities.Validators;
 
 /// <summary>
 /// A validator for the entity <see cref="UserProfile"/>
@@ -10,5 +12,16 @@ public class UserProfileValidator : Validator<UserProfile>
     /// </summary>
     public UserProfileValidator()
     {
+        RuleFor(p => p.FullName)
+            .MaximumLength(64)
+            .When(f => f is not null);
+
+        RuleFor(p => p.Address)
+            .MaximumLength(128)
+            .When(l => l is not null);
+
+        RuleFor(p => p.Bio)
+            .MaximumLength(255)
+            .When(b => b is not null);
     }
 }
