fbb0d293b1510ddabecaa6f460f0e2a8f877b362
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:27 +0300

    Create separate types for sellable types (Products & Services)

---
+++ BDP.Domain.Entities/ProductReview.cs
@@ -0,0 +1,7 @@
+﻿namespace BDP.Domain.Entities;
+
+/// <summary>
+/// A class to represent a product review
+/// </summary>
+public sealed class ProductReview : SellableReview<Product>
+{ }
---
+++ BDP.Domain.Entities/ServiceReview.cs
@@ -0,0 +1,7 @@
+﻿namespace BDP.Domain.Entities;
+
+/// <summary>
+/// A class to represent a service review
+/// </summary>
+public sealed class ServiceReview : SellableReview<Service>
+{ }
