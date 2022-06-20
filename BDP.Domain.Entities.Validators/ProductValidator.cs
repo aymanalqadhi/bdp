45fc8eaa1f5ce48b134ba17ee59a570659ae0bb6
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:44:39 +0300

    Add a validator for Product entity

---
+++ BDP.Domain.Entities.Validators/ProductValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace BDP.Domain.Entities.Validators;
+
+/// <summary>
+/// A validator for the entity <see cref="Product"/>
+/// </summary>
+public class ProductValidator : Validator<Product>
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public ProductValidator()
+    {
+        RuleFor(s => s.Title)
+            .NotEmpty()
+            .WithMessage("title cannot be empty");
+
+        RuleFor(s => s.Description)
+            .NotEmpty()
+            .WithMessage("description cannot be empty");
+    }
+}
