c776ca325978897a68e920e24831700c17d3de8c
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-21 00:40:00 +0300

    Refactor interface to have only with method with customizable options

--- BDP.Domain.Services.Interfaces/IEmailService.cs
+++ BDP.Domain.Services.Interfaces/IEmailService.cs
@@ -10,15 +10,17 @@ public interface IEmailService
     /// <param name="body">The body of the email message</param>
     /// <param name="attachments">The list of attachments to send along with the email</param>
     /// <returns></returns>
-    Task SendEmail(string to, string subject, string body, IList<FileStream>? attachments = null);
+    Task SendEmail(
+        string to,
+        string subject,
+        string body,
+        EmailOption options = EmailOption.None,
+        IEnumerable<FileStream>? attachments = null);
+}
 
-    /// <summary>
-    /// Asynchronously sends a html-formatted email to the supplied address
-    /// </summary>
-    /// <param name="to">The email to send to</param>
-    /// <param name="subject">The subject of the email message</param>
-    /// <param name="htmlBody">The html body of the email message</param>
-    /// <param name="attachments">The list of attachments to send along with the email</param>
-    /// <returns></returns>
-    Task SendHtmlEmail(string to, string subject, string htmlBody, IList<FileStream>? attachments = null);
+[Flags]
+public enum EmailOption : int
+{
+    None = 0,
+    HasHtmlBody = 1,
 }
