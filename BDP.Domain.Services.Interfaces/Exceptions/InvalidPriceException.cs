9fd5356109b3d82d66052aaf5b6a2e1ec52aba72
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 10:40:27 +0300

    Add a static method to encapsulate price validation logic

--- BDP.Domain.Services.Interfaces/Exceptions/InvalidPriceException.cs
+++ BDP.Domain.Services.Interfaces/Exceptions/InvalidPriceException.cs
@@ -2,7 +2,13 @@
 
 public class InvalidPriceException : Exception
 {
+    #region Fields
+
     private readonly decimal _price;
+
+    #endregion Fields
+
+    #region Public Constructors
 
     /// <summary>
     /// Default constructor
@@ -13,8 +19,29 @@ public class InvalidPriceException : Exception
         _price = price;
     }
 
+    #endregion Public Constructors
+
+    #region Properties
+
     /// <summary>
     /// Gets the price associated with this exception
     /// </summary>
     public decimal Price => _price;
+
+    #endregion Properties
+
+    #region Public Methods
+
+    /// <summary>
+    /// A static method to validate price values
+    /// </summary>
+    /// <param name="price">The price to validate</param>
+    /// <exception cref="InvalidPriceException"></exception>
+    public static void ValidatePrice(decimal price)
+    {
+        if (price <= 0 || price > 1_000_000)
+            throw new InvalidPriceException(price);
+    }
+
+    #endregion Public Methods
 }
