83ef3f09420da0940edcfe8afb76c67b486bb65f
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-23 00:43:08 +0300

    Reimplement events controller

--- BDP.Web.Api/Controllers/EventsController.cs
+++ BDP.Web.Api/Controllers/EventsController.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using BDP.Domain.Entities;
+﻿using BDP.Domain.Entities;
 using BDP.Domain.Repositories.Extensions;
 using BDP.Domain.Services;
 using BDP.Web.Api.Auth.Attributes;
@@ -7,9 +6,13 @@ using BDP.Web.Api.Extensions;
 using BDP.Web.Dtos;
 using BDP.Web.Dtos.Parameters;
 using BDP.Web.Dtos.Requests;
+
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Logging;
 
 namespace BDP.Web.Api.Controllers;
 
@@ -19,8 +22,6 @@ public class EventsController : ControllerBase
 {
     #region Private fields
 
-    private readonly IUsersService _usersSvc;
-    private readonly IPurchasesService _puchasesSvc;
     private readonly IEventsService _eventsSvc;
     private readonly IMapper _mapper;
 
@@ -29,13 +30,9 @@ public class EventsController : ControllerBase
     #region Ctors
 
     public EventsController(
-        IUsersService usersSvc,
-        IPurchasesService puchasesSvc,
         IEventsService eventsSvc,
         IMapper mapper)
     {
-        _usersSvc = usersSvc;
-        _puchasesSvc = puchasesSvc;
         _eventsSvc = eventsSvc;
         _mapper = mapper;
     }
@@ -44,10 +41,10 @@ public class EventsController : ControllerBase
 
     #region Actions
 
-    [HttpGet("{id}")]
+    [HttpGet("{eventId}")]
     [Authorize]
-    public async Task<IActionResult> GetById(Guid id)
-        => Ok(_mapper.Map<EventDto>(await _eventsSvc.GetByIdAsync(id)));
+    public async Task<IActionResult> GetById(EntityKey<Event> eventId)
+        => Ok(_mapper.Map<EventDto>(await _eventsSvc.GetEvents().FindAsync(eventId)));
 
     [HttpGet("types")]
     [Authorize]
@@ -62,9 +59,11 @@ public class EventsController : ControllerBase
 
     [HttpGet]
     [Authorize]
-    public IAsyncEnumerable<EventDto> GetEvents([Required] Guid userId, [FromQuery] PagingParameters paging)
+    public IAsyncEnumerable<EventDto> GetEvents(
+        [Required] EntityKey<User> userId,
+        [FromQuery] PagingParameters paging)
     {
-        var ret = _eventsSvc.ForUserAsync(userId)
+        var ret = _eventsSvc.ForUser(userId)
             .PageDescending(paging.Page, paging.PageLength)
             .Include(e => e.Pictures)
             .Include(e => e.Type)
@@ -89,16 +88,18 @@ public class EventsController : ControllerBase
         return Ok(_mapper.Map<EventDto>(ret));
     }
 
-    [HttpPatch("{id}")]
+    [HttpPatch("{eventId}")]
     [IsCustomer]
-    public async Task<IActionResult> Update(Guid id, [FromBody] UpdateEventRequest form)
+    public async Task<IActionResult> Update(
+        EntityKey<Event> eventId,
+        [FromBody] UpdateEventRequest form)
     {
-        var @event = await _eventsSvc.GetByIdAsync(id);
+        var @event = await _eventsSvc.GetEvents().FindAsync(eventId);
 
         if (@event.CreatedBy.Id != User.GetId())
             return Unauthorized(new { message = "you do not own the event" });
 
-        var type = await _eventsSvc.GetTypeByIdAsync(form.EventTypeId);
+        var type = await _eventsSvc.GetEventTypes().FindAsync(form.EventTypeId);
         var ret = await _eventsSvc.UpdateAsync(
             @event.Id,
             type.Id,
@@ -110,14 +111,14 @@ public class EventsController : ControllerBase
         return Ok(_mapper.Map<EventDto>(ret));
     }
 
-    [HttpDelete("{id}")]
+    [HttpDelete("{eventId}")]
     [IsCustomer]
-    public async Task<IActionResult> Delete(Guid id)
+    public async Task<IActionResult> Delete(EntityKey<Event> eventId)
     {
         // TODO:
         // move ownership logic to the service
 
-        var @event = await _eventsSvc.GetByIdAsync(id);
+        var @event = await _eventsSvc.GetEvents().FindAsync(eventId);
 
         if (@event.CreatedBy.Id != User.GetId())
             return Unauthorized(new { message = "you do not own the event" });
@@ -127,51 +128,16 @@ public class EventsController : ControllerBase
         return Ok();
     }
 
-    [HttpGet("{id}/purchases")]
+    [HttpPost("{eventId}/images")]
     [IsCustomer]
-    public async Task<IActionResult> GetAssociatePurchase(Guid id)
-    {
-        var user = await _usersSvc.GetByUsername(User.GetUsername()).FirstAsync();
-        var @event = await _eventsSvc.GetByIdAsync(id);
-
-        if (@event.CreatedBy.Id != user.Id)
-            return Unauthorized(new { message = "you do not own the event" });
-
-        return Ok(@event.Purchases.Select(_mapper.Map<PurchaseDto>));
-    }
-
-    [HttpPost("{id}/purchases")]
-    [IsCustomer]
-    public async Task<IActionResult> AssociatePurchase(
-        Guid id,
-        [FromBody] AssociatePurchaseWithEventRequest form)
+    public async Task<IActionResult> AddImage(
+        EntityKey<Event> eventId,
+        [FromForm] AddImageToEventRequest form)
     {
         // TODO:
         // move ownership logic to the service
 
-        var @event = await _eventsSvc.GetByIdAsync(id);
-
-        if (@event.CreatedBy.Id != User.GetId())
-            return Unauthorized(new { message = "you do not own the event" });
-
-        var purchase = await _puchasesSvc.GetById(form.PurchaseId);
-
-        if (purchase.Transaction.From.Id != User.GetId())
-            return Unauthorized(new { message = "you did not make the purchase" });
-
-        await _eventsSvc.AssociatePurchaseAsync(@event.Id, purchase.Id);
-
-        return Ok();
-    }
-
-    [HttpPost("{id}/images")]
-    [IsCustomer]
-    public async Task<IActionResult> AddImage(Guid id, [FromForm] AddImageToEventRequest form)
-    {
-        // TODO:
-        // move ownership logic to the service
-
-        var @event = await _eventsSvc.GetByIdAsync(id);
+        var @event = await _eventsSvc.GetEvents().FindAsync(eventId);
 
         if (@event.CreatedBy.Id != User.GetId())
             return Unauthorized(new { message = "you do not own the event" });
@@ -184,16 +150,16 @@ public class EventsController : ControllerBase
         return Ok();
     }
 
-    [HttpPut("{id}/progress")]
+    [HttpPut("{eventId}/progress")]
     [IsCustomer]
     public async Task<IActionResult> UpdateProgress(
-        Guid id,
+        EntityKey<Event> eventId,
         [FromBody] UpdateEventProgressRequess form)
     {
         // TODO:
         // move ownership logic to the service
 
-        var @event = await _eventsSvc.GetByIdAsync(id);
+        var @event = await _eventsSvc.GetEvents().FindAsync(eventId);
 
         if (@event.CreatedBy.Id != User.GetId())
             return Unauthorized(new { message = "you do not own the event" });
