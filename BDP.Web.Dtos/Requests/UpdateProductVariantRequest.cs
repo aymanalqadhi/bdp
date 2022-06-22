bbfd2ccedea109f306d94aefd5d47d387bdd509a
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 12:20:47 +0300

    Add a request class for product variants updating

---
+++ BDP.Web.Dtos/Requests/UpdateProductVariantRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace BDP.Web.Dtos.Requests;
+
+public sealed class UpdateProductVariantRequest
+{
+    /// <summary>
+    /// Gets or sets the name of the variant
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the description of the variant
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the price of the variant
+    /// </summary>
+    public decimal Price { get; set; }
+}
