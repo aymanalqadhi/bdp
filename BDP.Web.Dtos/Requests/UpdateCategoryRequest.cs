b91d9011e0f2f6557e67052f47b7243c335e112a
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 05:50:38 +0300

    Add a request for updating categories

---
+++ BDP.Web.Dtos/Requests/UpdateCategoryRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BDP.Web.Dtos.Requests;
+
+public class UpdateCategoryRequest
+{
+    /// <summary>
+    /// Gets or sets the name of the cateogry
+    /// </summary>
+    [Required]
+    public string Name { get; set; } = null!;
+}
