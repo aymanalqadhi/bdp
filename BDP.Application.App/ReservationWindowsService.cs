3a29f7ca34237af15e38617c416bdea2ec0d20d0
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 11:15:03 +0300

    Create a service to manage reservable product variants reservation windows

---
+++ BDP.Application.App/ReservationWindowsService.cs
@@ -0,0 +1,74 @@
+﻿using BDP.Domain.Entities;
+using BDP.Domain.Repositories;
+using BDP.Domain.Repositories.Extensions;
+using BDP.Domain.Services;
+using BDP.Domain.Services.Exceptions;
+
+namespace BDP.Application.App;
+
+/// <summary>
+/// A service to manage reservable product variants reservation windows
+/// </summary>
+public sealed class ReservationWindowsService : IReservationWindowsService
+{
+    #region Fields
+
+    private readonly IUnitOfWork _uow;
+
+    #endregion Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="uow">The unit of work of the application</param>
+    public ReservationWindowsService(IUnitOfWork uow)
+    {
+        _uow = uow;
+    }
+
+    #endregion Public Constructors
+
+    #region Public Methods
+
+    /// <inheritdoc/>
+    public async Task<ReservationWindow> AddReservationWindowAsync(
+        EntityKey<ProductVariant> variantId,
+        Weekday weekdays,
+        TimeOnly start,
+        TimeOnly end)
+    {
+        var variant = await _uow.ProductVariants.Query().FindAsync(variantId);
+
+        if (variant.Type != ProductVariantType.Reservable)
+            throw new InvalidProductVaraintTypeException(variantId, ProductVariantType.Reservable, variant.Type);
+
+        var window = new ReservationWindow
+        {
+            AvailableDays = weekdays,
+            Start = start,
+            End = end,
+        };
+
+        _uow.ReservationWindows.Add(window);
+        await _uow.CommitAsync();
+
+        return window;
+    }
+
+    /// <inheritdoc/>
+    public async Task RemoveReservationWindowAsync(EntityKey<ReservationWindow> windowId)
+    {
+        var window = await _uow.ReservationWindows.Query().FindAsync(windowId);
+
+        _uow.ReservationWindows.Remove(window);
+        await _uow.CommitAsync();
+    }
+
+    /// <inheritdoc/>
+    public IQueryBuilder<ReservationWindow> ReservationWindowsFor(EntityKey<ProductVariant> variantId)
+        => _uow.ReservationWindows.Query().Where(w => w.Variant.Id == variantId);
+
+    #endregion Public Methods
+}
---
+++ BDP.Domain.Services.Interfaces/IReservationWindowsService.cs
@@ -0,0 +1,46 @@
+﻿using BDP.Domain.Entities;
+using BDP.Domain.Repositories;
+
+namespace BDP.Domain.Services;
+
+/// <summary>
+/// An interface to be implemented by reservable product variants reservation
+/// windows services
+/// </summary>
+public interface IReservationWindowsService
+{
+    #region Public Methods
+
+    /// <summary>
+    /// Asynchrnously adds a reservation window for a reservable variant
+    /// </summary>
+    /// <param name="variantId">The id of the variant</param>
+    /// <param name="weekdays">The weekdays of reservation window</param>
+    /// <param name="start">The start of the reservation </param>
+    /// <param name="end"></param>
+    /// <returns>The created reservation window</returns>
+    /// <exception cref="InvalidProductVaraintTypeException"></exception>
+    Task<ReservationWindow> AddReservationWindowAsync(
+        EntityKey<ProductVariant> variantId,
+        Weekday weekdays,
+        TimeOnly start,
+        TimeOnly end);
+
+    /// <summary>
+    /// Asynchrnously removes a reservable product variant reservation window
+    /// </summary>
+    /// <param name="windowId"></param>
+    /// <returns></returns>
+    Task RemoveReservationWindowAsync(EntityKey<ReservationWindow> windowId);
+
+    /// <summary>
+    /// Gets resrvation windows for a reservable product variant
+    /// </summary>
+    /// <param name="variantId">
+    /// The id of the product variant which to get the reservation windows for
+    /// </param>
+    /// <returns>A query builder for the reservation windows </returns>
+    IQueryBuilder<ReservationWindow> ReservationWindowsFor(EntityKey<ProductVariant> variantId);
+
+    #endregion Public Methods
+}
