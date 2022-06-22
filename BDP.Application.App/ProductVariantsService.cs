5930e8e6474c9c28a3b6f30c5352f214312fe4e6
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 05:53:19 +0300

    Implement product variants service

---
+++ BDP.Application.App/ProductVariantsService.cs
@@ -0,0 +1,26 @@
+﻿using BDP.Domain.Entities;
+using BDP.Domain.Repositories;
+using BDP.Domain.Services;
+
+namespace BDP.Application.App;
+
+/// <summary>
+/// A service to manage product variants
+/// </summary>
+public sealed class ProductVariantsService : IProductVariantsService
+{
+    private readonly IUnitOfWork _uow;
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="uow">The unit of work of the application</param>
+    public ProductVariantsService(IUnitOfWork uow)
+    {
+        _uow = uow;
+    }
+
+    /// <inheritdoc/>
+    public IQueryBuilder<ProductVariant> GetVariants()
+        => _uow.ProductVariants.Query();
+}
