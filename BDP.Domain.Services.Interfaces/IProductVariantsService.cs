b5188be04994f0e56a3551ca9fa917c65fb8edb8
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 05:53:08 +0300

    Add an interface for product variant service

---
+++ BDP.Domain.Services.Interfaces/IProductVariantsService.cs
@@ -0,0 +1,16 @@
+﻿using BDP.Domain.Entities;
+using BDP.Domain.Repositories;
+
+namespace BDP.Domain.Services;
+
+/// <summary>
+/// An interface to be implemented by product variants services
+/// </summary>
+public interface IProductVariantsService
+{
+    /// <summary>
+    /// Gets product variants
+    /// </summary>
+    /// <returns></returns>
+    IQueryBuilder<ProductVariant> GetVariants();
+}
