1dc4b2f0017e6a569c541d43d3c2f8dfe19b8c26
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 12:21:24 +0300

    Add a data-transfer object for StockBatch entity

---
+++ BDP.Web.Dtos/StockBatchDto.cs
@@ -0,0 +1,14 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Web.Dtos;
+
+/// <summary>
+/// A data-transfer object for <see cref="StockBatch"/> with sellable type
+/// </summary>
+public sealed class StockBatchDto : EntityDto<StockBatch>
+{
+    /// <summary>
+    /// Gets or sets the quantity of the stock batch
+    /// </summary>
+    public uint Quantity { get; set; }
+}
