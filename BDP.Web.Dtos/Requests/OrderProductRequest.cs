7ed8929ec4abe072e62ba3381bb2a401dd099532
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 12:37:39 +0300

    Rename ProductOrder to a more clear name Order

--- BDP.Web.Dtos/Requests/OrderProductRequest.cs
+++ BDP.Web.Dtos/Requests/OrderProductRequest.cs
@@ -2,7 +2,7 @@
 
 namespace BDP.Web.Dtos.Requests;
 
-public class OrderProductRequest
+public class OrderRequest
 {
     [Required]
     [Range(1, 100, ErrorMessage = "you cannot order less than 1 or more than 100 products")]
