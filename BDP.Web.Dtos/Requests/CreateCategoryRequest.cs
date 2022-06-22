a5839e49030e728244e16ecbae86e64d9c0ab28c
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 05:51:56 +0300

    Add a request class for categories creation

---
+++ BDP.Web.Dtos/Requests/CreateCategoryRequest.cs
@@ -0,0 +1,18 @@
+﻿using BDP.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace BDP.Web.Dtos.Requests;
+
+public class CreateCategoryRequest
+{
+    /// <summary>
+    /// Gets or sets the name of the cateogry
+    /// </summary>
+    [Required]
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// An optional parent id
+    /// </summary>
+    public EntityKey<Category>? Parent { get; set; }
+}
