cca567ba3ce153e6cdc379ead0850e754ab9b660
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 12:20:23 +0300

    Add a request class for reservation window creation

---
+++ BDP.Web.Dtos/Requests/CreateReservationWindowRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace BDP.Web.Dtos.Requests;
+
+public class CreateReservationWindowRequest
+{
+    /// <summary>
+    /// Gets or sets the available days of the reservable product
+    /// </summary>
+    public byte AvailableDays { get; set; }
+
+    /// <summary>
+    /// Gets or sets the starting time of reservable product availability
+    /// </summary>
+    public TimeSpan Start { get; set; }
+
+    /// <summary>
+    /// Gets or sets the ending time of reservable product availability
+    /// </summary>
+    public TimeSpan End { get; set; }
+}
