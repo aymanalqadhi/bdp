aedaf7e56baade9babcd00347f990a8e6a45abf4
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 12:20:32 +0300

    Add a request class for stock batches creation

---
+++ BDP.Web.Dtos/Requests/CreateStockBatchRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BDP.Web.Dtos.Requests;
+
+public class CreateStockBatchRequest
+{
+    /// <summary>
+    /// Gets or sets the quantity of the stock batch
+    /// </summary>
+    public uint Quantity { get; set; }
+}
