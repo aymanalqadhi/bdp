9c314e20f6434018caf9c5bcc7ff27cd835b3d86
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-22 05:50:20 +0300

    Add a data-transfer object for Category

---
+++ BDP.Web.Dtos/CategoryDto.cs
@@ -0,0 +1,19 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Web.Dtos;
+
+/// <summary>
+/// A dto for <see cref="Category"/>
+/// </summary>
+public sealed class CategoryDto : EntityDto<Category>
+{
+    /// <summary>
+    /// Gets or sets the name of the category
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the sub-categories of the category
+    /// </summary>
+    public IEnumerable<CategoryDto> Children { get; set; } = new List<CategoryDto>();
+}
