0cddc11e1e7f72d85350dbe457370fb8f8e86e3f
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-24 02:29:40 +0300

    Add type conversions for TimeOnly properties

--- BDP.Infrastructure.Repositories.EntityFramework/Configuration/ReservationWindowTypeConfiguration.cs
+++ BDP.Infrastructure.Repositories.EntityFramework/Configuration/ReservationWindowTypeConfiguration.cs
@@ -1,8 +1,20 @@
 ﻿using BDP.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BDP.Infrastructure.Repositories.EntityFramework.Configuration;
 
 /// <inheritdoc/>
 public sealed class ReservationWindowTypeConfiguration : EntityTypeConfiguration<ReservationWindow>
 {
+    /// <inheritdoc/>
+    public override void Configure(EntityTypeBuilder<ReservationWindow> builder)
+    {
+        base.Configure(builder);
+
+        // type conversion
+        builder.Property(w => w.Start)
+            .HasConversion(s => s.ToTimeSpan(), t => TimeOnly.FromTimeSpan(t));
+        builder.Property(w => w.End)
+            .HasConversion(s => s.ToTimeSpan(), t => TimeOnly.FromTimeSpan(t));
+    }
 }
