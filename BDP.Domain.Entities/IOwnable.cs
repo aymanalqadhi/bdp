3ea0bddcc3ee2f3cffa1a2bc6c8c1a161780791e
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-25 06:13:25 +0300

    Add an interface to be implemented by ownable entities

---
+++ BDP.Domain.Entities/IOwnable.cs
@@ -0,0 +1,12 @@
+﻿namespace BDP.Domain.Entities;
+
+/// <summary>
+/// An interface to be implemented by ownable entities
+/// </summary>
+public interface IOwnable
+{
+    /// <summary>
+    /// Gets the user who owns the resoruce
+    /// </summary>
+    User OwnedBy { get; }
+}
