e31edede0a248c00d2d4057f69b264ceda940480
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-25 08:43:47 +0300

    Add PurchaseAlreadyEarlyConfirmedException subtypes for purhcase types

---
+++ BDP.Domain.Services.Interfaces/Exceptions/OrderAlreadyEarlyConfirmedException.cs
@@ -0,0 +1,15 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Domain.Services.Exceptions;
+
+/// <summary>
+/// An exception thrown when an order is already early-confimred by the provider
+/// </summary>
+public sealed class OrderAlreadyEarlyConfirmedException : PurchaseAlreadyEarlyConfirmedException<Order>
+{
+    /// <inheritdoc/>
+    public OrderAlreadyEarlyConfirmedException(EntityKey<Order> purchaseId)
+        : base(purchaseId, $"order #{purchaseId} already early-confirmed")
+    {
+    }
+}
---
+++ BDP.Domain.Services.Interfaces/Exceptions/ReservationAlreadyEarlyConfirmedException.cs
@@ -0,0 +1,15 @@
+﻿using BDP.Domain.Entities;
+
+namespace BDP.Domain.Services.Exceptions;
+
+/// <summary>
+/// An exception thrown when an order is already early-confimred by the provider
+/// </summary>
+public sealed class ReservationAlreadyEarlyConfirmedException : PurchaseAlreadyEarlyConfirmedException<Reservation>
+{
+    /// <inheritdoc/>
+    public ReservationAlreadyEarlyConfirmedException(EntityKey<Reservation> purchaseId)
+        : base(purchaseId, $"order #{purchaseId} already early-confirmed")
+    {
+    }
+}
