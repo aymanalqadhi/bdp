72f644819e8dbff2fb5647d265becea0f85b3429
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-25 06:25:03 +0300

    Add an interface to be implemented by permissions services

---
+++ BDP.Domain.Services.Interfaces/IPermissionsService.cs
@@ -0,0 +1,16 @@
+﻿using BDP.Domain.Entities;
+using BDP.Domain.Repositories;
+
+namespace BDP.Domain.Services;
+
+public interface IPermissionsService
+{
+    Task<bool> HasRoleAsync(EntityKey<User> userId, UserRole role);
+
+    Task<TResult?> GetIfOwnedAsync<TEntity, TResult>(
+        EntityKey<User> userId,
+        EntityKey<TEntity> resourceId,
+        Func<IQueryBuilder<TEntity>, IQueryBuilder<TResult>>? queryConfiguration = null)
+        where TEntity : class
+        where TResult : class;
+}
