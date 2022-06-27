ada75c01568afe37627474d316acecf1cfd8d7f7
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-27 09:16:42 +0300

    Update products service interface to include ownership details

--- BDP.Domain.Services.Interfaces/IProductsService.cs
+++ BDP.Domain.Services.Interfaces/IProductsService.cs
@@ -48,11 +48,12 @@ public interface IProductsService
     /// <summary>
     /// Asynchronously removes a product
     /// </summary>
+    /// <param name="userId">The id of the user owning the product</param>
     /// <param name="productId">The id of the product to remove</param>
     /// <param name="cancelPurchases">If true, cancel all pending purchases of the product</param>
     /// <returns></returns>
     /// <exception cref="PendingPurchasesLeftException"></exception>
-    Task RemoveAsync(EntityKey<Product> productId, bool cancelPurchases = false);
+    Task RemoveAsync(EntityKey<User> userId, EntityKey<Product> productId, bool cancelPurchases = false);
 
     /// <summary>
     /// Searches products gloablly
@@ -72,18 +73,16 @@ public interface IProductsService
     /// <summary>
     /// Asynchrnonously updates a product
     /// </summary>
+    /// <param name="userId">The id of the user owning the product</param>
     /// <param name="productId">The id of the product to update</param>
     /// <param name="title">The title of the product</param>
     /// <param name="description">The description of the product</param>
     /// <returns>The updated product</returns>
-    Task<Product> UpdateAsync(EntityKey<Product> productId, string title, string description);
-
-    /// <summary>
-    /// Asynchrnonously removes a product
-    /// </summary>
-    /// <param name="productId">the product to remove</param>
-    /// <returns></returns>
-    Task RemoveAsync(EntityKey<Product> productId);
+    Task<Product> UpdateAsync(
+        EntityKey<User> userId,
+        EntityKey<Product> productId,
+        string title,
+        string description);
 
     #endregion Public Methods
 }
