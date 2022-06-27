eb034443b3eefd8c6ee3ea40f6545480f935d94a
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-27 09:50:07 +0300

    Update categories interface to include user id

--- BDP.Domain.Services.Interfaces/ICategoriesService.cs
+++ BDP.Domain.Services.Interfaces/ICategoriesService.cs
@@ -17,16 +17,18 @@ public interface ICategoriesService
     /// <summary>
     /// Asynchronously adds a product cateogry
     /// </summary>
+    /// <param name="userId">The id of the user to add the category with</param>
     /// <param name="name">the name of the category</param>
     /// <param name="parent">The id of the parent of the category (optional)</param>
     /// <returns>The created category object</returns>
-    Task<Category> AddAsync(string name, EntityKey<Category>? parent = null);
+    Task<Category> AddAsync(EntityKey<User> userId, string name, EntityKey<Category>? parent = null);
 
     /// <summary>
     /// Asynchrnously updates a category
     /// </summary>
+    /// <param name="userId">The id of the user to update the category with</param>
     /// <param name="categoryId">The id of the category to update</param>
     /// <param name="name">The new name of the category</param>
     /// <returns>The updated category</returns>
-    Task<Category> UpdateAsync(EntityKey<Category> categoryId, string name);
+    Task<Category> UpdateAsync(EntityKey<User> userId, EntityKey<Category> categoryId, string name);
 }
