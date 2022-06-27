95e90569fb355f621b42b5e794c9eeb90a6988c7
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-27 09:49:25 +0300

    Add a method to securly get the confirmation token of a transaction

--- BDP.Domain.Services.Interfaces/ITransactionsService.cs
+++ BDP.Domain.Services.Interfaces/ITransactionsService.cs
@@ -9,12 +9,31 @@ namespace BDP.Domain.Services;
 /// </summary>
 public interface ITransactionsService
 {
+    #region Public Methods
+
     /// <summary>
-    /// Asynchronously gets a transaction by id
+    /// Asynchronously cancels a transaction
     /// </summary>
-    /// <param name="id">The id of the transaction</param>
-    /// <returns>The transaction with the passed id</returns>
-    IQueryBuilder<Transaction> GetById(EntityKey<Transaction> id);
+    /// <param name="receiverId">The id of the receiver of the transaction</param>
+    /// <param name="transactionId">The id of the transaction to cancel</param>
+    /// <returns>The canceled transaction confirmation</returns>
+    /// <exception cref="TransactionAlreadyConfirmedException"></exception>
+    Task<TransactionConfirmation> CancelAsync(
+        EntityKey<User> receiverId,
+        EntityKey<Transaction> transactionId);
+
+    /// <summary>
+    /// Asynchronously confirms a transaction
+    /// </summary>
+    /// <param name="senderId">The id of the sender of the transaction</param>
+    /// <param name="transactionId">The id of the transaction to confirm</param>
+    /// <param name="confirmationToken">The token to confirm with</param>
+    /// <returns>The transaction confirmation</returns>
+    /// <exception cref="TransactionAlreadyConfirmedException"></exception>
+    Task<TransactionConfirmation> ConfirmAsync(
+        EntityKey<User> senderId,
+        EntityKey<Transaction> transactionId,
+        string confirmationToken);
 
     /// <summary>
     /// Asynchrnously gets transactions that a user has either sent or recieved
@@ -23,6 +42,21 @@ public interface ITransactionsService
     /// <param name="userId">The id of the user which to get transactions for</param>
     /// <returns></returns>
     IQueryBuilder<Transaction> ForUserAsync(EntityKey<User> userId);
+
+    /// <summary>
+    /// Asynchronously gets a transaction by id
+    /// </summary>
+    /// <param name="id">The id of the transaction</param>
+    /// <returns>The transaction with the passed id</returns>
+    IQueryBuilder<Transaction> GetById(EntityKey<Transaction> id);
+
+    /// <summary>
+    /// Asynchronously gets confirmation token of a transaction
+    /// </summary>
+    /// <param name="userId">The id of the owner of the transaction (from)</param>
+    /// <param name="transactionId">The id of the transaction to get its confirmation</param>
+    /// <returns>The confirmation token of the transaction</returns>
+    Task<string> GetConfirmationTokenAsync(EntityKey<User> userId, EntityKey<Transaction> transactionId);
 
     /// <summary>
     /// Asynchronously gets the total input transferred to the user
@@ -47,27 +81,5 @@ public interface ITransactionsService
     /// <returns>Total usable credit</returns>
     Task<decimal> TotalUsableAsync(EntityKey<User> userId);
 
-    /// <summary>
-    /// Asynchronously confirms a transaction
-    /// </summary>
-    /// <param name="senderId">The id of the sender of the transaction</param>
-    /// <param name="transactionId">The id of the transaction to confirm</param>
-    /// <param name="confirmationToken">The token to confirm with</param>
-    /// <returns>The transaction confirmation</returns>
-    /// <exception cref="TransactionAlreadyConfirmedException"></exception>
-    Task<TransactionConfirmation> ConfirmAsync(
-        EntityKey<User> senderId,
-        EntityKey<Transaction> transactionId,
-        string confirmationToken);
-
-    /// <summary>
-    /// Asynchronously cancels a transaction
-    /// </summary>
-    /// <param name="receiverId">The id of the receiver of the transaction</param>
-    /// <param name="transactionId">The id of the transaction to cancel</param>
-    /// <returns>The canceled transaction confirmation</returns>
-    /// <exception cref="TransactionAlreadyConfirmedException"></exception>
-    Task<TransactionConfirmation> CancelAsync(
-        EntityKey<User> receiverId,
-        EntityKey<Transaction> transactionId);
+    #endregion Public Methods
 }
