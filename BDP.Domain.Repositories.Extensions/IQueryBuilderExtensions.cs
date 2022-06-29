d838e4b5030d3fade7e97b5bc28ffe7a98955674
Ayman Al-Qadhi <alqd@protonmail.com>
2022-06-29 11:29:44 +0300

    Add an extension method to apply filtering when a condition applies

--- BDP.Domain.Repositories.Extensions/IQueryBuilderExtensions.cs
+++ BDP.Domain.Repositories.Extensions/IQueryBuilderExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using BDP.Domain.Entities;
 using BDP.Domain.Repositories.Extensions.Exceptions;
 
+using System.Linq.Expressions;
+
 namespace BDP.Domain.Repositories.Extensions;
 
 public static class IQueryBuilderExtensions
@@ -9,15 +11,15 @@ public static class IQueryBuilderExtensions
     /// Orders the current query ascendingly
     /// </summary>
     /// <returns>The modified query builder</returns>
-    public static IQueryBuilder<T> Order<T>(this IQueryBuilder<T> source) where T : AuditableEntity<T>
-        => source.OrderBy(x => x.CreatedAt);
+    public static IQueryBuilder<T> Order<T>(this IQueryBuilder<T> self) where T : AuditableEntity<T>
+        => self.OrderBy(x => x.CreatedAt);
 
     /// <summary>
     /// Orders the current query descendingly
     /// </summary>
     /// <returns>The modified query builder</returns>
-    public static IQueryBuilder<T> OrderDescending<T>(this IQueryBuilder<T> source) where T : AuditableEntity<T>
-        => source.OrderByDescending(x => x.CreatedAt);
+    public static IQueryBuilder<T> OrderDescending<T>(this IQueryBuilder<T> self) where T : AuditableEntity<T>
+        => self.OrderByDescending(x => x.CreatedAt);
 
     /// <summary>
     /// Fetches the page with index <see cref="page"/> and size <see cref="pageLength"/>
@@ -26,13 +28,13 @@ public static class IQueryBuilderExtensions
     /// <param name="pageLength">The length of the page</param>
     /// <returns>The modified query builder</returns>
     /// <exception cref="InvalidPaginationParametersException"></exception>
-    public static IQueryBuilder<T> Page<T>(this IQueryBuilder<T> source, int page, int pageLength)
+    public static IQueryBuilder<T> Page<T>(this IQueryBuilder<T> self, int page, int pageLength)
         where T : AuditableEntity<T>
     {
         if (page <= 0 || pageLength <= 0)
             throw new InvalidPaginationParametersException(page, pageLength);
 
-        return source
+        return self
             .Skip((page - 1) * pageLength)
             .Take(page * pageLength);
     }
@@ -45,10 +47,10 @@ public static class IQueryBuilderExtensions
     /// <param name="pageLength">The length of the page</param>
     /// <returns>The modified query builder</returns>
     /// <exception cref="InvalidPaginationParametersException"></exception>
-    public static IQueryBuilder<T> PageDescending<T>(this IQueryBuilder<T> source, int page, int pageLength)
+    public static IQueryBuilder<T> PageDescending<T>(this IQueryBuilder<T> self, int page, int pageLength)
         where T : AuditableEntity<T>
     {
-        return source
+        return self
             .OrderDescending()
             .Page(page, pageLength);
     }
@@ -60,11 +62,11 @@ public static class IQueryBuilderExtensions
     /// <param name="id">The id of the item to get</param>
     /// <returns>The matched item</returns>
     public static Task<T> FindAsync<T>(
-        this IQueryBuilder<T> source,
+        this IQueryBuilder<T> self,
         EntityKey<T> id,
         CancellationToken cancellationToken = default) where T : AuditableEntity<T>
     {
-        return source.FirstAsync(i => i.Id == id, cancellationToken);
+        return self.FirstAsync(i => i.Id == id, cancellationToken);
     }
 
     /// <summary>
@@ -74,10 +76,27 @@ public static class IQueryBuilderExtensions
     /// <param name="id">The id of the item to get</param>
     /// <returns>The matched item if found, null otherwise</returns>
     public static Task<T?> FindOrDefaultAsync<T>(
-        this IQueryBuilder<T> source,
+        this IQueryBuilder<T> self,
         EntityKey<T> id,
         CancellationToken cancellationToken = default) where T : AuditableEntity<T>
     {
-        return source.FirstOrDefaultAsync(i => i.Id == id, cancellationToken);
+        return self.FirstOrDefaultAsync(i => i.Id == id, cancellationToken);
+    }
+
+    /// <summary>
+    /// Applies a filter if the passed condition is true
+    /// found, null is returned.
+    /// </summary>
+    /// <param name="expr">The predicate expression to be applied as a filter</param>
+    /// <param name="cond">The condition upon which to apply the filter</param>
+    /// <returns>
+    /// A filtered query builder if <paramref name="cond"/> is true, unmodified otheriwse
+    /// </returns>
+    public static IQueryBuilder<T> WhereIf<T>(
+        this IQueryBuilder<T> self,
+        Expression<Func<T, bool>> expr,
+        bool cond) where T : AuditableEntity<T>
+    {
+        return cond ? self.Where(expr) : self;
     }
 }
